2bc010abcb11d9a51b8c6877f11e9dca4a60690f
Vaclav Elias <vaclavelias@gmail.com>
2023-09-11 00:28:44 +0100

    feat: Examples improvements

---
+++ src/Stride.CommunityToolkit.Examples/Example.cs
@@ -0,0 +1,15 @@
+namespace Stride.CommunityToolkit.Examples;
+
+public class Example
+{
+    public string Id { get; set; }
+    public string Title { get; set; }
+    public Action Action { get; set; }
+
+    public Example(string id, string title, Action action)
+    {
+        Id = id;
+        Title = title;
+        Action = action;
+    }
+}
--- src/Stride.CommunityToolkit.Examples/Program.cs
+++ src/Stride.CommunityToolkit.Examples/Program.cs
@@ -1,32 +1,61 @@
 using Pastel;
+using Stride.CommunityToolkit.Examples;
 using Stride.Examples.Models;
+using System.Diagnostics;
 using System.Drawing;
 
-Console.WriteLine($"{Navigation("[1]")} Basic Example - Give me a cube");
-Console.WriteLine($"{Navigation("[2]")} Basic Example - Capsule with rigid body");
-Console.WriteLine($"{Navigation("[3]")} Basic Example - Capsule with rigid body and window");
-Console.WriteLine($"{Navigation("[4]")} Basic Example - Profiler");
-Console.WriteLine();
-Console.WriteLine($"{Navigation("[Q]")} Quit");
-Console.WriteLine();
+var _examples = GetExamples();
+
+DisplayMenu();
 
 while (true)
 {
     Console.WriteLine($"Enter choice and press {"ENTER".Pastel(Color.FromArgb(165, 229, 250))} to continue");
 
-    var choice = Console.ReadLine();
+    var choice = Console.ReadLine() ?? "";
 
-    switch (choice)
+    var example = _examples.FirstOrDefault(x => string.Equals(x.Id, choice, StringComparison.OrdinalIgnoreCase));
+
+    if (example is null)
     {
-        case "1": GiveMeACubeExample.Run(); break;
-        case "2": CapsuleExample.Run(); break;
-        case "3": CapsuleAndWindowExample.Run(); break;
-        case "q": return;
-        case "Q": return;
+        Console.WriteLine("Invalid choice. Try again.".Pastel(Color.Red));
+    }
+    else
+    {
+        example.Action();
     }
 }
 
-string Navigation(string text)
+void DisplayMenu()
 {
-    return text.Pastel(Color.LightGreen);
+    Console.SetCursorPosition(0, 0);
+
+    Console.WriteLine("Stride Community Toolkit Examples".Pastel(Color.LightBlue));
+    Console.WriteLine();
+
+    foreach (var example in _examples)
+    {
+        Console.WriteLine($"{Navigation($"[{example.Id}]")} {example.Title}");
+    }
+
+    Console.WriteLine();
 }
+
+static string Navigation(string text) => text.Pastel(Color.LightGreen);
+
+static void StartProcess(string projectName) => Process.Start(new ProcessStartInfo
+{
+    FileName = $"{projectName}.exe",
+    RedirectStandardOutput = true,
+    UseShellExecute = false,
+    CreateNoWindow = true,
+});
+
+static List<Example> GetExamples() => new() {
+    new Example("1", "Basic Example - Capsule with rigid body", () => StartProcess(nameof(Example01_Basic3DScene))),
+    new Example("2", "Basic Example - Give me a cube", () => StartProcess(nameof(Example02_GiveMeACube))),
+    new Example("3", "Basic Example - Capsule with rigid body and window", () => CapsuleAndWindowExample.Run()),
+    //new Example("4", "Basic Example - Profiler",
+    //                  () => StartProcess(nameof(Example04_Profiler))),
+    new Example("Q", "Quit", () => Environment.Exit(0))
+};
