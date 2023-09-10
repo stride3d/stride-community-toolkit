51caa896a7515030fb9ef0ed8b42cc89892444f6
Doprez <73259914+Doprez@users.noreply.github.com>
2023-09-10 10:58:41 -0600

    added Doprez extensions

--- src/Stride.CommunityToolkit/Extensions/CameraComponentExtensions.cs
+++ src/Stride.CommunityToolkit/Extensions/CameraComponentExtensions.cs
@@ -31,5 +31,33 @@ public static class CameraComponentExtensions
         vectorFar /= vectorFar.W;
 
         return (vectorNear, vectorFar);
-    }
+	}
+	public static Vector3 LogicDirectionToWorldDirection(this CameraComponent camera, Vector2 logicDirection, Vector3 upVector)
+	{
+		camera.Update();
+		var inverseView = Matrix.Invert(camera.ViewMatrix);
+
+		var forward = Vector3.Cross(upVector, inverseView.Right);
+		forward.Normalize();
+
+		var right = Vector3.Cross(forward, upVector);
+		var worldDirection = forward * logicDirection.Y + right * logicDirection.X;
+		worldDirection.Normalize();
+		return worldDirection;
+	}
+
+	public static Vector3 LogicDirectionToWorldDirection(this CameraComponent camera, Vector2 logicDirection)
+	{
+		camera.Update();
+		var upVector = Vector3.UnitY;
+		var inverseView = Matrix.Invert(camera.ViewMatrix);
+
+		var forward = Vector3.Cross(upVector, inverseView.Right);
+		forward.Normalize();
+
+		var right = Vector3.Cross(forward, upVector);
+		var worldDirection = forward * logicDirection.Y + right * logicDirection.X;
+		worldDirection.Normalize();
+		return worldDirection;
+	}
 }
---
+++ src/Stride.CommunityToolkit/Extensions/EntityComponentExtensions.cs
@@ -0,0 +1,56 @@
+﻿namespace Stride.Engine;
+
+public static class EntityComponentExtensions
+{
+	/// <summary>
+	/// Definitely not a rip off of Unity's GetComponent.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <param name="entity"></param>
+	/// <returns></returns>
+	public static T GetComponent<T>(this Entity entity)
+	{
+		var result = entity.OfType<T>().FirstOrDefault();
+		return result;
+	}
+
+	/// <summary>
+	/// Gets all components of type T
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	/// <param name="entity"></param>
+	/// <returns></returns>
+	public static IEnumerable<T> GetComponents<T>(this Entity entity)
+	{
+		var result = entity.OfType<T>();
+		return result;
+	}
+
+	/// <summary>
+	/// Destroys the entity that calls this method.
+	/// </summary>
+	/// <param name="entity"></param>
+	/// <returns></returns>
+	public static bool DestroyEntity(this Entity entity)
+	{
+		try
+		{
+			entity.Scene.Entities.Remove(entity);
+			return true;
+		}
+		catch
+		{
+			return false;
+		}
+	}
+
+	/// <summary>
+	/// an easier way to get world position rather than Transform.WorldMatrix.TranslationVector
+	/// </summary>
+	/// <param name="entity"></param>
+	/// <returns></returns>
+	public static Vector3 WorldPosition(this Entity entity)
+	{
+		return entity.Transform.WorldMatrix.TranslationVector;
+	}
+}
