92189b3eeeb208e824abd7449d4309e1552d3224
Vaclav Elias <vaclavelias@gmail.com>
2023-09-11 23:27:39 +0100

    feat: Example project 3 added

---
+++ examples/code-only/Example03_CapsuleAndWindow/NamespaceAnchor.cs
@@ -0,0 +1,6 @@
+namespace Example03_CapsuleAndWindow;
+
+/// <summary>
+/// This empty class is here to make the namespace available in the nameof() operator in the main examples project.
+/// </summary>
+internal class NamespaceAnchor { }
---
+++ examples/code-only/Example03_CapsuleAndWindow/Program.cs
@@ -0,0 +1,77 @@
+using Stride.CommunityToolkit.Extensions;
+using Stride.CommunityToolkit.ProceduralModels;
+using Stride.Core.Mathematics;
+using Stride.Engine;
+using Stride.Graphics;
+using Stride.Rendering;
+using Stride.UI;
+using Stride.UI.Controls;
+using Stride.UI.Panels;
+
+SpriteFont? _font;
+
+using var game = new Game();
+
+game.Run(start: Start);
+
+void Start(Scene rootScene)
+{
+    game.SetupBase3DScene();
+
+    AddCapsule(rootScene);
+
+    LoadFont();
+
+    AddWindow(rootScene);
+}
+
+void AddCapsule(Scene rootScene)
+{
+    var entity = game.CreatePrimitive(PrimitiveModelType.Capsule);
+
+    entity.Transform.Position = new Vector3(0, 8, 0);
+
+    entity.Scene = rootScene;
+}
+
+void LoadFont()
+{
+    _font = game.Content.Load<SpriteFont>("StrideDefaultFont");
+}
+
+void AddWindow(Scene rootScene)
+{
+    var uiEntity = CreateUIEntity();
+
+    uiEntity.Scene = rootScene;
+}
+
+Entity CreateUIEntity()
+{
+    return new Entity
+    {
+        new UIComponent
+        {
+            Page = new UIPage { RootElement = CreateCanvas() },
+            RenderGroup = RenderGroup.Group31
+        }
+    };
+}
+
+Canvas CreateCanvas()
+{
+    var canvas = new Canvas { Width = 300, Height = 100, BackgroundColor = new Color(248, 177, 149, 100) };
+
+    canvas.Children.Add(CreateTextBlock(_font));
+
+    return canvas;
+}
+
+TextBlock CreateTextBlock(SpriteFont? _font) => new TextBlock
+{
+    Text = "Hello, World",
+    TextColor = Color.White,
+    TextSize = 20,
+    Margin = new Thickness(3, 3, 3, 0),
+    Font = _font
+};
