c557d2f32b435e0963577b2678464ff663b36e2a
Vaclav Elias <vaclavelias@gmail.com>
2023-09-17 23:15:57 +0100

    feat: F# example added

---
+++ examples/code-only/Example05_FSharp_Basic3DScene/Program.fs
@@ -0,0 +1,19 @@
+open Stride.CommunityToolkit.Extensions;
+open Stride.CommunityToolkit.ProceduralModels;
+open Stride.Core.Mathematics;
+open Stride.Engine;
+
+let game = new Game()
+
+let Start rootScene =
+    game.SetupBase3DScene()
+    game.AddProfiler() |> ignore
+
+    let firstBox = game.CreatePrimitive(PrimitiveModelType.Cube);
+    firstBox.Transform.Position <- new Vector3(0f, 2.5f, 0f)
+    firstBox.Scene <- rootScene
+
+[<EntryPoint>]
+let main argv =
+    game.Run(start = Start)
+    0
