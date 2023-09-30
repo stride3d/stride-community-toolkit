4a7502a4d31154f22843216994609b00681ea4a5
Vaclav Elias <vaclavelias@gmail.com>
2023-09-30 22:47:16 +0100

    feat: All example projects included

--- src/Stride.CommunityToolkit.Examples/ExampleProvider.cs
+++ src/Stride.CommunityToolkit.Examples/ExampleProvider.cs
@@ -4,15 +4,32 @@ namespace Stride.CommunityToolkit.Examples;
 
 public static class ExampleProvider
 {
+    private static int _index = 0;
+
     public static List<Example> GetExamples() => new()
     {
-        new Example("1", "Basic Example - Capsule with rigid body", () => StartProcess(nameof(Example01_Basic3DScene))),
-        new Example("2", "Basic Example - Capsule with rigid body in F#", () => StartProcess(nameof(Example05_FSharp_Basic3DScene))),
-        new Example("3", "Basic Example - Give me a cube", () => StartProcess(nameof(Example02_GiveMeACube))),
-        new Example("4", "Basic Example - Stride UI - Capsule with rigid body and Window", () => StartProcess(nameof(Example03_StrideUI_CapsuleAndWindow))),
-        //new Example("5", "Basic Example - Myra UI - Draggable Window, GetService()", () => StartProcess(nameof(Example04_MyraUI))),
+        new Example(GetIndex(), "Basic Example - Capsule with rigid body",
+            () => StartProcess(nameof(Example01_Basic3DScene))),
+
+        new Example(GetIndex(), "Basic Example - Capsule with rigid body in F#",
+            () => StartProcess(nameof(Example01_Basic3DScene_FSharp))),
+
+        new Example(GetIndex(), "Basic Example - Capsule with rigid body in Visual Basic",
+            () => StartProcess(nameof(Example01_Basic3DScene_VBasic))),
+
+        new Example(GetIndex(), "Basic Example - Give me a cube",
+            () => StartProcess(nameof(Example02_GiveMeACube))),
+
+        new Example(GetIndex(), "Basic Example - Stride UI - Capsule with rigid body and Window",
+            () => StartProcess(nameof(Example03_StrideUI_CapsuleAndWindow))),
+
+        new Example(GetIndex(), "Basic Example - Myra UI - Draggable Window, GetService()",
+            () => StartProcess(nameof(Example04_MyraUI))),
+
         new Example("Q", "Quit", () => Environment.Exit(0))
     };
+
+    private static string GetIndex() => Interlocked.Increment(ref _index).ToString();
 
     private static void StartProcess(string projectName) => Process.Start(new ProcessStartInfo
     {
