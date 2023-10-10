a80a7ade1021e7c402fa92df757b5e198e006338
Doprez <73259914+Doprez@users.noreply.github.com>
2023-10-10 10:04:38 -0600

    added new Eflag value to Raycast extensions

--- src/Stride.CommunityToolkit/Extensions/SimulationExtensions.cs
+++ src/Stride.CommunityToolkit/Extensions/SimulationExtensions.cs
@@ -1,4 +1,5 @@
 using Stride.Engine;
+using static BulletSharp.TriangleRaycastCallback;
 
 namespace Stride.Physics;
 
@@ -8,13 +9,13 @@ public static class SimulationExtensions
     /// A Raycast method based on the example in the fps demo
     /// <para>Make sure you are using the actual rotating Entity otherwise you will waste hours like I did debuging a non issue</para>
     /// </summary>
-    public static HitResult RayCast(this Simulation simulation, Entity entityPosition, float length = 1, CollisionFilterGroupFlags collisionFlags = CollisionFilterGroupFlags.AllFilter)
+    public static HitResult RayCast(this Simulation simulation, Entity entityPosition, float length = 1, CollisionFilterGroupFlags collisionFlags = CollisionFilterGroupFlags.AllFilter, EFlags eFlags = EFlags.None)
     {
         var raycastStart = entityPosition.Transform.WorldMatrix.TranslationVector;
         var forward = entityPosition.Transform.WorldMatrix.Forward;
         var raycastEnd = raycastStart + forward * length;
 
-        var result = simulation.Raycast(raycastStart, raycastEnd, filterFlags: collisionFlags);
+        var result = simulation.Raycast(raycastStart, raycastEnd, filterFlags: collisionFlags, eFlags: eFlags);
 
         return result;
     }
@@ -22,12 +23,12 @@ public static class SimulationExtensions
     /// <summary>
     /// A Raycast method based on the example in the fps demo
     /// </summary>
-    public static HitResult RayCast(this Simulation simulation, Entity entityPosition, Vector3 direction, float length = 1, CollisionFilterGroupFlags collisionFlags = CollisionFilterGroupFlags.AllFilter)
+    public static HitResult RayCast(this Simulation simulation, Entity entityPosition, Vector3 direction, float length = 1, CollisionFilterGroupFlags collisionFlags = CollisionFilterGroupFlags.AllFilter, EFlags eFlags = EFlags.None)
     {
         var raycastStart = entityPosition.Transform.WorldMatrix.TranslationVector;
         var raycastEnd = raycastStart + direction * length;
 
-        var result = simulation.Raycast(raycastStart, raycastEnd, filterFlags: collisionFlags);
+        var result = simulation.Raycast(raycastStart, raycastEnd, filterFlags: collisionFlags, eFlags: eFlags);
 
         return result;
     }
