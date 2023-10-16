75d082860b95b30e39a9e3ceab163797cc901e26
Alexander Schill <alexander@schillnet.de>
2023-10-16 02:16:30 +0200

    Add documentation as well as a life updates

--- examples/code-only/Example05_ProceduralGeometry/Program.cs
+++ examples/code-only/Example05_ProceduralGeometry/Program.cs
@@ -2,12 +2,17 @@ using Stride.CommunityToolkit.Engine;
 using Stride.CommunityToolkit.Rendering.Utilities;
 using Stride.Core.Mathematics;
 using Stride.Engine;
+using Stride.Games;
 using Stride.Graphics;
 using Stride.Rendering;
+using Stride.Rendering.Materials;
+using Stride.Rendering.Materials.ComputeColors;
 
 using var game = new Game();
 
-game.Run(start: Start);
+Entity? circleEntity = null;
+game.Run(start: Start, update: Update);
+
 
 void Start(Scene rootScene)
 {
@@ -15,24 +20,33 @@ void Start(Scene rootScene)
 
     AddMesh(game.GraphicsDevice, rootScene, Vector3.Zero, GiveMeATriangle);
     AddMesh(game.GraphicsDevice, rootScene, Vector3.UnitX * 2, GiveMeAPlane);
-    AddMesh(game.GraphicsDevice, rootScene, Vector3.UnitX * -2, GiveMeACircle);
+}
+void Update(Scene rootScene, GameTime gameTime)
+{
+    var segments = (int)((Math.Cos(gameTime.Total.TotalMilliseconds / 500) + 1) / 2 * 47) + 3;
+    circleEntity?.DestroyEntity();
+    circleEntity = AddMesh(game.GraphicsDevice, rootScene, Vector3.UnitX * -2, b => GiveMeACircle(b, segments));
 }
 
 void GiveMeATriangle(MeshBuilder meshBuilder)
 {
-    meshBuilder.WithIndexType(IndexingType.Int32);
+    meshBuilder.WithIndexType(IndexingType.Int16);
     meshBuilder.WithPrimitiveType(PrimitiveType.TriangleList);
 
     var position = meshBuilder.WithPosition<Vector3>();
+    var color = meshBuilder.WithColor<Color>();
 
     meshBuilder.AddVertex();
     meshBuilder.SetElement(position, new Vector3(0, 0, 0));
+    meshBuilder.SetElement(color, Color.Red);
 
     meshBuilder.AddVertex();
     meshBuilder.SetElement(position, new Vector3(1, 0, 0));
+    meshBuilder.SetElement(color, Color.Green);
 
     meshBuilder.AddVertex();
     meshBuilder.SetElement(position, new Vector3(.5f, 1, 0));
+    meshBuilder.SetElement(color, Color.Blue);
 
     meshBuilder.AddIndex(0);
     meshBuilder.AddIndex(2);
@@ -41,22 +55,27 @@ void GiveMeATriangle(MeshBuilder meshBuilder)
 
 void GiveMeAPlane(MeshBuilder meshBuilder)
 {
-    meshBuilder.WithIndexType(IndexingType.Int32);
+    meshBuilder.WithIndexType(IndexingType.Int16);
     meshBuilder.WithPrimitiveType(PrimitiveType.TriangleList);
 
     var position = meshBuilder.WithPosition<Vector3>();
+    var color = meshBuilder.WithColor<Color>();
 
     meshBuilder.AddVertex();
     meshBuilder.SetElement(position, new Vector3(0, 0, 0));
+    meshBuilder.SetElement(color, Color.Red);
 
     meshBuilder.AddVertex();
     meshBuilder.SetElement(position, new Vector3(0, 1, 0));
+    meshBuilder.SetElement(color, Color.Green);
 
     meshBuilder.AddVertex();
     meshBuilder.SetElement(position, new Vector3(1, 1, 0));
+    meshBuilder.SetElement(color, Color.Blue);
 
     meshBuilder.AddVertex();
     meshBuilder.SetElement(position, new Vector3(1, 0, 0));
+    meshBuilder.SetElement(color, Color.Yellow);
 
     meshBuilder.AddIndex(0);
     meshBuilder.AddIndex(1);
@@ -67,25 +86,28 @@ void GiveMeAPlane(MeshBuilder meshBuilder)
     meshBuilder.AddIndex(3);
 }
 
-void GiveMeACircle(MeshBuilder meshBuilder)
+void GiveMeACircle(MeshBuilder meshBuilder, int segments)
 {
-    meshBuilder.WithIndexType(IndexingType.Int32);
+    meshBuilder.WithIndexType(IndexingType.Int16);
     meshBuilder.WithPrimitiveType(PrimitiveType.TriangleList);
 
     var position = meshBuilder.WithPosition<Vector3>();
+    var color = meshBuilder.WithColor<Color4>();
 
-    const int segments = 48;
     for (var i = 0; i < segments; i++)
     {
         var x = (float)Math.Sin(Math.Tau / segments * i) / 2;
         var y = (float)Math.Cos(Math.Tau / segments * i) / 2;
+        var hsl = new ColorHSV(360f / segments * i, 1, 1, 1).ToColor();
 
         meshBuilder.AddVertex();
         meshBuilder.SetElement(position, new Vector3(x + .5f, y + .5f, 0));
+        meshBuilder.SetElement(color, hsl);
     }
 
     meshBuilder.AddVertex();
     meshBuilder.SetElement(position, new Vector3(.5f, .5f, 0));
+    meshBuilder.SetElement(color, Color.Black.ToColor4());
 
     for (var i = 0; i < segments; i++)
     {
@@ -95,7 +117,7 @@ void GiveMeACircle(MeshBuilder meshBuilder)
     }
 }
 
-void AddMesh(GraphicsDevice graphicsDevice, Scene rootScene, Vector3 position, Action<MeshBuilder> build)
+Entity AddMesh(GraphicsDevice graphicsDevice, Scene rootScene, Vector3 position, Action<MeshBuilder> build)
 {
     using var meshBuilder = new MeshBuilder();
     build(meshBuilder);
@@ -103,10 +125,21 @@ void AddMesh(GraphicsDevice graphicsDevice, Scene rootScene, Vector3 position, A
     var entity = new Entity { Scene = rootScene, Transform = { Position = position } };
     var model = new Model
     {
+        new MaterialInstance {
+            Material = Material.New(graphicsDevice, new MaterialDescriptor {
+                Attributes = new MaterialAttributes {
+                    DiffuseModel = new MaterialDiffuseLambertModelFeature(),
+                    Diffuse = new MaterialDiffuseMapFeature {
+                        DiffuseMap = new ComputeVertexStreamColor()
+                    },
+                }
+            })
+        },
         new Mesh {
             Draw = meshBuilder.ToMeshDraw(graphicsDevice),
             MaterialIndex = 0
         }
     };
     entity.Add(new ModelComponent { Model = model });
+    return entity;
 }
