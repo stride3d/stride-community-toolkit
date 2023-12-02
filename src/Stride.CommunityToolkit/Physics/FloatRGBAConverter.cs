757ce6a336cc6e115ef468f906f6e0eb7421030a
Vaclav Elias <vaclavelias@gmail.com>
2023-12-02 23:12:29 +0000

    refactor: Minor HeightmapExtensions refactoring

---
+++ src/Stride.CommunityToolkit/Physics/FloatRGBAConverter.cs
@@ -0,0 +1,42 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Stride.CommunityToolkit.Physics;
+
+public static partial class HeightmapExtensions
+{
+    /// <summary>
+    /// Float from bytes and back, like the Union structure from c++
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit)]
+    public struct FloatRGBAConverter
+    {
+        [FieldOffset(0)]
+        public float Float;
+
+        [FieldOffset(0)]
+        public byte R;
+
+        [FieldOffset(1)]
+        public byte G;
+
+        [FieldOffset(2)]
+        public byte B;
+
+        [FieldOffset(3)]
+        public byte A;
+
+        public FloatRGBAConverter(float @float) : this()
+        {
+            Float = @float;
+        }
+
+        public FloatRGBAConverter(byte r, byte g, byte b, byte a) : this()
+        {
+            R = r;
+            G = g;
+            B = b;
+            A = a;
+        }
+    }
+
+}
---
+++ src/Stride.CommunityToolkit/Physics/VertexTypePosTexNormColor.cs
@@ -0,0 +1,78 @@
+﻿using Stride.Graphics;
+using System.Runtime.InteropServices;
+
+namespace Stride.CommunityToolkit.Physics;
+
+public static partial class HeightmapExtensions
+{
+    /// <summary>
+    /// Custom vertex type so that we can generate tangents for supporting normal maps
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential, Pack = 4)]
+    public struct VertexTypePosTexNormColor : IEquatable<VertexTypePosTexNormColor>, IVertex
+    {
+        public VertexTypePosTexNormColor(Vector3 position, Vector3 normal, Vector3 tangent,
+            Vector2 texCoord1, Vector4 color1) : this()
+        {
+            Position = position;
+            Normal = normal;
+            Tangent = tangent;
+            TexCoord = texCoord1;
+            Color = color1;
+        }
+
+        public Vector3 Position;
+        public Vector3 Normal;
+        public Vector3 Tangent;
+        public Vector2 TexCoord;
+        public Vector4 Color;
+
+        public static readonly int Size = 60;
+
+        public static readonly VertexDeclaration Layout = new VertexDeclaration(
+           VertexElement.Position<Vector3>(),//12=4*3
+           VertexElement.Normal<Vector3>(),//24
+           VertexElement.Tangent<Vector3>(),//36
+           VertexElement.TextureCoordinate<Vector2>(),//44
+           VertexElement.Color<Vector4>());
+
+        public bool Equals(VertexTypePosTexNormColor other)
+            => Position.Equals(other.Position) && Normal.Equals(other.Normal) && Tangent.Equals(other.Tangent)
+            && Color.Equals(other.Color) && TexCoord.Equals(other.TexCoord);
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            return obj is VertexTypePosTexNormColor && Equals((VertexTypePosTexNormColor)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hashCode = Position.GetHashCode();
+                hashCode = (hashCode * 397) ^ Normal.GetHashCode();
+                hashCode = (hashCode * 397) ^ TexCoord.GetHashCode();
+                hashCode = (hashCode * 397) ^ Color.GetHashCode();
+                return hashCode;
+            }
+        }
+
+        public VertexDeclaration GetLayout()
+            => Layout;
+
+        public void FlipWinding()
+            => TexCoord.X = (1.0f - TexCoord.X);
+
+        public static bool operator ==(VertexTypePosTexNormColor left, VertexTypePosTexNormColor right)
+            => left.Equals(right);
+
+        public static bool operator !=(VertexTypePosTexNormColor left, VertexTypePosTexNormColor right)
+            => !left.Equals(right);
+
+        public override string ToString()
+            => string.Format("Position: {0}, Normal: {1}, Tangent {2}, Texcoord: {3}, Color: {4}", Position, Normal, Tangent, TexCoord, Color);
+
+    }
+
+}
