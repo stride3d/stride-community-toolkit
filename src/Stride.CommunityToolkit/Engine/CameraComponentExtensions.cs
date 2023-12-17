6e48c456ec80df559715f251577ae32ee337faa7
Vaclav Elias <vaclavelias@gmail.com>
2023-12-17 22:12:56 +0000

    refactor: CameraComponentExtensions code improvements and related docs improvements

--- src/Stride.CommunityToolkit/Engine/CameraComponentExtensions.cs
+++ src/Stride.CommunityToolkit/Engine/CameraComponentExtensions.cs
@@ -97,9 +97,9 @@ public static class CameraComponentExtensions
     /// <param name="component">The ScriptComponent which has the Simulation to run the Cast in.</param>
     /// <param name="screenPosition">The screen position (in world coordinates) where the ray should be cast through.</param>
     /// <param name="collisionGroups">Optional. The collision filter group to consider during the raycasting. Default is CollisionFilterGroups.DefaultFilter.</param>
-    /// <param name="collisionFlags">Optional. The collision filter group flags to consider during the raycasting. Default is CollisionFilterGroupFlags.DefaultFilter.</param>
+    /// <param name="collisionFilterGroupFlags">Optional. The collision filter group flags to consider during the raycasting. Default is CollisionFilterGroupFlags.DefaultFilter.</param>
     /// <returns>A HitResult containing information about the hit result, including the hit location and other collision data.</returns>
-    public static HitResult Raycast(this CameraComponent camera, ScriptComponent component, Vector2 screenPosition, CollisionFilterGroups collisionGroups = CollisionFilterGroups.DefaultFilter, CollisionFilterGroupFlags collisionFlags = CollisionFilterGroupFlags.DefaultFilter)
+    public static HitResult Raycast(this CameraComponent camera, ScriptComponent component, Vector2 screenPosition, CollisionFilterGroups collisionGroups = CollisionFilterGroups.DefaultFilter, CollisionFilterGroupFlags collisionFilterGroupFlags = CollisionFilterGroupFlags.DefaultFilter)
     {
         var invertedMatrix = Matrix.Invert(camera.ViewProjectionMatrix);
 
@@ -126,16 +126,30 @@ public static class CameraComponentExtensions
         vectorFar /= vectorFar.W;
 
         // Raycast from the point on the near plane to the point on the far plane and get the collision result
-        return component.GetSimulation().Raycast(vectorNear.XYZ(), vectorFar.XYZ(), collisionGroups, collisionFlags);
+        return component.GetSimulation().Raycast(vectorNear.XYZ(), vectorFar.XYZ(), collisionGroups, collisionFilterGroupFlags);
     }
 
     /// <summary>
-    /// Returns near and far vector based on a ray going from camera through a screen point. The ray is in world space, starting on the near plane of the camera and going through position's (x,y) pixel coordinates on the screen.
+    /// Performs a raycasting operation from the specified CameraComponent's position through the mouse cursor position in screen coordinates,
+    /// and returns information about the hit result.
     /// </summary>
-    /// <param name="camera"></param>
-    /// <param name="screenPosition"></param>
-    /// <returns></returns>
-    // screenPosition or mousePosition
+    /// <param name="camera">The CameraComponent from which the ray should be cast.</param>
+    /// <param name="component">The ScriptComponent from which the Input.MousePosition should be taken.</param>
+    /// <param name="collisionGroups">Optional. The collision filter group to consider during the raycasting. Default is CollisionFilterGroups.DefaultFilter.</param>
+    /// <param name="collisionFilterGroupFlags">Optional. The collision filter group flags to consider during the raycasting. Default is CollisionFilterGroupFlags.DefaultFilter.</param>
+    /// <returns>A HitResult containing information about the hit result, including the hit location and other collision data.</returns>
+    public static HitResult RaycastMouse(this CameraComponent camera, ScriptComponent component, CollisionFilterGroups collisionGroups = CollisionFilterGroups.DefaultFilter, CollisionFilterGroupFlags collisionFilterGroupFlags = CollisionFilterGroupFlags.DefaultFilter)
+    {
+        return camera.Raycast(component, component.Input.MousePosition, collisionGroups, collisionFilterGroupFlags);
+    }
+
+    /// <summary>
+    /// Calculates the near and far vectors for a ray that starts at the camera and passes through a given screen point.
+    /// The ray is in world space, starting at the near plane of the camera and extending through the specified pixel coordinates on the screen.
+    /// </summary>
+    /// <param name="camera">The camera component used to calculate the ray.</param>
+    /// <param name="screenPosition">The screen position (in normalized coordinates) through which the ray passes.</param>
+    /// <returns>A tuple containing the near vector (<c>VectorNear</c>) and the far vector (<c>VectorFar</c>) of the ray in world space.</returns>
     public static (Vector4 VectorNear, Vector4 VectorFar) ScreenPointToRay(this CameraComponent camera, Vector2 screenPosition)
     {
         var invertedMatrix = Matrix.Invert(camera.ViewProjectionMatrix);
@@ -156,18 +170,94 @@ public static class CameraComponentExtensions
         return (vectorNear, vectorFar);
     }
 
+    // ToDo: A benchmark would be useful if we should use out or just directly return the Vector3, that means one of these methods migth be unnecessary?
     /// <summary>
-    /// Performs a raycasting operation from the specified CameraComponent's position through the mouse cursor position in screen coordinates,
-    /// and returns information about the hit result.
+    /// Converts the screen position to a point in world coordinates.
     /// </summary>
-    /// <param name="camera">The CameraComponent from which the ray should be cast.</param>
-    /// <param name="component">The ScriptComponent from which the Input.MousePosition should be taken.</param>
-    /// <param name="collisionGroup">Optional. The collision filter group to consider during the raycasting. Default is CollisionFilterGroups.DefaultFilter.</param>
-    /// <param name="collisionFilterGroupFlags">Optional. The collision filter group flags to consider during the raycasting. Default is CollisionFilterGroupFlags.DefaultFilter.</param>
-    /// <returns>A HitResult containing information about the hit result, including the hit location and other collision data.</returns>
-    public static HitResult RaycastMouse(this CameraComponent camera, ScriptComponent component, CollisionFilterGroups collisionGroup = CollisionFilterGroups.DefaultFilter, CollisionFilterGroupFlags collisionFilterGroupFlags = CollisionFilterGroupFlags.DefaultFilter)
+    /// <param name="cameraComponent"></param>
+    /// <param name="position">The screen position in normalized X, Y coordinates. Top-left is (0,0), bottom-right is (1,1). Z is in world units from near camera plane.</param>
+    /// <returns>Position in world coordinates.</returns>
+    /// <exception cref="ArgumentNullException">If the cameraComponent argument is <see langword="null"/>.</exception>
+    /// <remarks>
+    /// This method does not update the <see cref="CameraComponent.ViewMatrix"/> or <see cref="CameraComponent.ProjectionMatrix"/> before performing the transformation.
+    /// If the <see cref="CameraComponent"/> or it's containing <see cref="Entity"/> <see cref="TransformComponent"/>has been modified since the last frame you may need to call the <see cref="CameraComponent.Update()"/> method first.
+    /// </remarks>
+    public static Vector3 ScreenToWorldPoint(this CameraComponent cameraComponent, Vector3 position)
     {
-        return camera.Raycast(component, component.Input.MousePosition, collisionGroup, collisionFilterGroupFlags);
+        cameraComponent.ScreenToWorldPoint(ref position, out var result);
+
+        return result;
+    }
+
+    /// <summary>
+    /// Converts the screen position to a point in world coordinates.
+    /// </summary>
+    /// <param name="cameraComponent"></param>
+    /// <param name="position">The screen position in normalized X, Y coordinates. Top-left is (0,0), bottom-right is (1,1). Z is in world units from near camera plane.</param>
+    /// <param name="result">Position in world coordinates.</param>
+    /// <remarks>
+    /// This method does not update the <see cref="CameraComponent.ViewMatrix"/> or <see cref="CameraComponent.ProjectionMatrix"/> before performing the transformation.
+    /// If the <see cref="CameraComponent"/> or it's containing <see cref="Entity"/> <see cref="TransformComponent"/>has been modified since the last frame you may need to call the <see cref="CameraComponent.Update()"/> method first.
+    /// </remarks>
+    public static void ScreenToWorldPoint(this CameraComponent cameraComponent, ref Vector3 position, out Vector3 result)
+    {
+        var position2D = position.XY();
+
+        cameraComponent.ScreenToWorldRaySegment(ref position2D, out var ray);
+
+        var direction = ray.End - ray.Start;
+        direction.Normalize();
+
+        Vector3.TransformNormal(ref direction, ref cameraComponent.ViewMatrix, out var viewSpaceDir);
+
+        float rayDistance = position.Z / viewSpaceDir.Z;
+
+        result = ray.Start + direction * rayDistance;
+    }
+
+    /// <summary>
+    /// Converts the screen position to a <see cref="RaySegment"/> in world coordinates.
+    /// </summary>
+    /// <param name="cameraComponent"></param>
+    /// <param name="position"></param>
+    /// <returns><see cref="RaySegment"/>, starting at near plain and ending at the far plain.</returns>
+    /// <exception cref="ArgumentNullException">If the cameraComponent argument is <see langword="null"/>.</exception>
+    /// <remarks>
+    /// This method does not update the <see cref="CameraComponent.ViewMatrix"/> or <see cref="CameraComponent.ProjectionMatrix"/> before performing the transformation.
+    /// If the <see cref="CameraComponent"/> or it's containing <see cref="Entity"/> <see cref="TransformComponent"/>has been modified since the last frame you may need to call the <see cref="CameraComponent.Update()"/> method first.
+    /// </remarks>
+    public static RaySegment ScreenToWorldRaySegment(this CameraComponent cameraComponent, Vector2 position)
+    {
+        cameraComponent.ScreenToWorldRaySegment(ref position, out var result);
+
+        return result;
+    }
+
+    /// <summary>
+    /// Converts the screen position to a <see cref="RaySegment"/> in world coordinates.
+    /// </summary>
+    /// <param name="cameraComponent"></param>
+    /// <param name="position"></param>
+    /// <param name="result"><see cref="RaySegment"/>, starting at near plain and ending at the far plain.</param>
+    /// <remarks>
+    /// This method does not update the <see cref="CameraComponent.ViewMatrix"/> or <see cref="CameraComponent.ProjectionMatrix"/> before performing the transformation.
+    /// If the <see cref="CameraComponent"/> or it's containing <see cref="Entity"/> <see cref="TransformComponent"/>has been modified since the last frame you may need to call the <see cref="CameraComponent.Update()"/> method first.
+    /// </remarks>
+    public static void ScreenToWorldRaySegment(this CameraComponent cameraComponent, ref Vector2 position, out RaySegment result)
+    {
+        ArgumentNullException.ThrowIfNull(cameraComponent);
+
+        Matrix.Invert(ref cameraComponent.ViewProjectionMatrix, out var inverseViewProjection);
+
+        ScreenToClipSpace(ref position, out var clipSpace);
+
+        Vector3.TransformCoordinate(ref clipSpace, ref inverseViewProjection, out var near);
+
+        clipSpace.Z = 1f;
+
+        Vector3.TransformCoordinate(ref clipSpace, ref inverseViewProjection, out var far);
+
+        result = new RaySegment(near, far);
     }
 
     /// <summary>
@@ -232,51 +322,6 @@ public static class CameraComponentExtensions
         };
     }
 
-    /// <summary>
-    /// Converts the screen position to a <see cref="RaySegment"/> in world coordinates.
-    /// </summary>
-    /// <param name="cameraComponent"></param>
-    /// <param name="position"></param>
-    /// <returns><see cref="RaySegment"/>, starting at near plain and ending at the far plain.</returns>
-    /// <exception cref="ArgumentNullException">If the cameraComponent argument is <see langword="null"/>.</exception>
-    /// <remarks>
-    /// This method does not update the <see cref="CameraComponent.ViewMatrix"/> or <see cref="CameraComponent.ProjectionMatrix"/> before performing the transformation.
-    /// If the <see cref="CameraComponent"/> or it's containing <see cref="Entity"/> <see cref="TransformComponent"/>has been modified since the last frame you may need to call the <see cref="CameraComponent.Update()"/> method first.
-    /// </remarks>
-    public static RaySegment ScreenToWorldRaySegment(this CameraComponent cameraComponent, Vector2 position)
-    {
-        cameraComponent.ScreenToWorldRaySegment(ref position, out var result);
-
-        return result;
-    }
-
-    /// <summary>
-    /// Converts the screen position to a <see cref="RaySegment"/> in world coordinates.
-    /// </summary>
-    /// <param name="cameraComponent"></param>
-    /// <param name="position"></param>
-    /// <param name="result"><see cref="RaySegment"/>, starting at near plain and ending at the far plain.</param>
-    /// <remarks>
-    /// This method does not update the <see cref="CameraComponent.ViewMatrix"/> or <see cref="CameraComponent.ProjectionMatrix"/> before performing the transformation.
-    /// If the <see cref="CameraComponent"/> or it's containing <see cref="Entity"/> <see cref="TransformComponent"/>has been modified since the last frame you may need to call the <see cref="CameraComponent.Update()"/> method first.
-    /// </remarks>
-    public static void ScreenToWorldRaySegment(this CameraComponent cameraComponent, ref Vector2 position, out RaySegment result)
-    {
-        if (cameraComponent == null)
-            throw new ArgumentNullException(nameof(cameraComponent));
-        Matrix.Invert(ref cameraComponent.ViewProjectionMatrix, out var inverseViewProjection);
-
-        ScreenToClipSpace(ref position, out var clipSpace);
-
-        Vector3.TransformCoordinate(ref clipSpace, ref inverseViewProjection, out var near);
-
-        clipSpace.Z = 1f;
-
-        Vector3.TransformCoordinate(ref clipSpace, ref inverseViewProjection, out var far);
-
-        result = new RaySegment(near, far);
-    }
-
     private static void ScreenToClipSpace(ref Vector2 position, out Vector3 clipSpace)
     {
         clipSpace = new Vector3
@@ -285,49 +330,5 @@ public static class CameraComponentExtensions
             Y = 1f - position.Y * 2f,
             Z = 0f
         };
-    }
-
-    /// <summary>
-    /// Converts the screen position to a point in world coordinates.
-    /// </summary>
-    /// <param name="cameraComponent"></param>
-    /// <param name="position">The screen position in normalized X, Y coordinates. Top-left is (0,0), bottom-right is (1,1). Z is in world units from near camera plane.</param>
-    /// <returns>Position in world coordinates.</returns>
-    /// <exception cref="ArgumentNullException">If the cameraComponent argument is <see langword="null"/>.</exception>
-    /// <remarks>
-    /// This method does not update the <see cref="CameraComponent.ViewMatrix"/> or <see cref="CameraComponent.ProjectionMatrix"/> before performing the transformation.
-    /// If the <see cref="CameraComponent"/> or it's containing <see cref="Entity"/> <see cref="TransformComponent"/>has been modified since the last frame you may need to call the <see cref="CameraComponent.Update()"/> method first.
-    /// </remarks>
-    public static Vector3 ScreenToWorldPoint(this CameraComponent cameraComponent, Vector3 position)
-    {
-        cameraComponent.ScreenToWorldPoint(ref position, out var result);
-
-        return result;
-    }
-
-    /// <summary>
-    /// Converts the screen position to a point in world coordinates.
-    /// </summary>
-    /// <param name="cameraComponent"></param>
-    /// <param name="position">The screen position in normalized X, Y coordinates. Top-left is (0,0), bottom-right is (1,1). Z is in world units from near camera plane.</param>
-    /// <param name="result">Position in world coordinates.</param>
-    /// <remarks>
-    /// This method does not update the <see cref="CameraComponent.ViewMatrix"/> or <see cref="CameraComponent.ProjectionMatrix"/> before performing the transformation.
-    /// If the <see cref="CameraComponent"/> or it's containing <see cref="Entity"/> <see cref="TransformComponent"/>has been modified since the last frame you may need to call the <see cref="CameraComponent.Update()"/> method first.
-    /// </remarks>
-    public static void ScreenToWorldPoint(this CameraComponent cameraComponent, ref Vector3 position, out Vector3 result)
-    {
-        var position2D = position.XY();
-
-        cameraComponent.ScreenToWorldRaySegment(ref position2D, out var ray);
-
-        var direction = ray.End - ray.Start;
-        direction.Normalize();
-
-        Vector3.TransformNormal(ref direction, ref cameraComponent.ViewMatrix, out var viewSpaceDir);
-
-        float rayDistance = position.Z / viewSpaceDir.Z;
-
-        result = ray.Start + direction * rayDistance;
     }
 }
