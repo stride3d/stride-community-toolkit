c0de3b03254c047521133d73bdc65a002fb2f4fb
Vaclav Elias <vaclavelias@gmail.com>
2024-01-08 23:11:11 +0000

    feat: Example updated

--- examples/code-only/Example01_Basic2DScene/Program.cs
+++ examples/code-only/Example01_Basic2DScene/Program.cs
@@ -19,17 +19,19 @@ using var game = new Game();
 
 var boxSize = new Vector3(0.2f);
 var rectangleSize = new Vector3(0.2f, 0.3f, 0);
-var squareModel = new Model();
-var rectangleModel = new Model();
-var circleModel = new Model();
-var triangleModel = new Model();
 int cubes = 0;
 int debugX = 5;
 int debugY = 30;
 Simulation? simulation = null;
 CameraComponent? _camera = null;
-Entity? _selectedEntity = null;
 Scene scene = new();
+
+List<ShapeModel> shapes = [
+    new() { Type = ShapeType.Square, Color = Color.Green, Size = boxSize },
+    new() { Type = ShapeType.Rectangle, Color = Color.Orange, Size = rectangleSize },
+    new() { Type = ShapeType.Circle, Color = Color.Red, Size = boxSize },
+    new() { Type = ShapeType.Triangle, Color = Color.Purple, Size = rectangleSize }
+];
 
 game.Run(start: Start, update: Update);
 
@@ -44,114 +46,71 @@ void Start(Scene rootScene)
     //game.SetupBase3DScene();
 
     game.AddGraphicsCompositor().AddCleanUIStage();
-
     game.Add3DCamera().AddInteractiveCameraScript();
     //game.Add2DCamera().AddInteractiveCameraScript();
 
-    game.AddDirectionalLight();
-    game.AddAllDirectionLighting(intensity: 20f, true);
+    //game.AddDirectionalLight();
+    game.AddAllDirectionLighting(intensity: 50f, true);
     game.AddSkybox();
 
     // Make sure you also update 2D Ground collider if you are testing this
     //game.Add2DGround();
     game.Add3DGround();
 
-    game.AddGroundGizmo(new(-2, 1, 0), showAxisName: true);
+    game.AddGroundGizmo(new(0, 0, -7.5f), showAxisName: true);
     game.AddProfiler();
 
     //AddSpriteBatchRenderer(rootScene);
 
     _camera = game.SceneSystem.SceneInstance.RootScene.Entities.FirstOrDefault(x => x.Get<CameraComponent>() != null)?.Get<CameraComponent>();
 
-    squareModel = new Model();
-    //squareModel.Materials.Add(game.CreateMaterial(Color.DarkGreen));
-    squareModel.Materials.Add(new MaterialInstance
-    {
-        Material = Material.New(game.GraphicsDevice, new MaterialDescriptor
-        {
-            Attributes = new MaterialAttributes
-            {
-                DiffuseModel = new MaterialDiffuseLambertModelFeature(),
-                Diffuse = new MaterialDiffuseMapFeature
-                {
-                    DiffuseMap = new ComputeVertexStreamColor()
-                },
-            }
-        })
-    });
-    squareModel.Meshes.Add(
-        new Mesh
-        {
-            Draw = GiveMeShape(boxSize, Color.DarkGreen).ToMeshDraw(game.GraphicsDevice),
-            MaterialIndex = 0
-        }
-    );
-
-    rectangleModel = new Model
-    {
-        new Mesh {
-            Draw = GiveMeShape(rectangleSize, Color.Orange).ToMeshDraw(game.GraphicsDevice),
-            //MaterialIndex = 0
-        },
-        new MaterialInstance
-    {
-        Material = Material.New(game.GraphicsDevice, new MaterialDescriptor
-        {
-            Attributes = new MaterialAttributes
-            {
-                DiffuseModel = new MaterialDiffuseLambertModelFeature(),
-                Diffuse = new MaterialDiffuseMapFeature
-                {
-                    DiffuseMap = new ComputeVertexStreamColor()
-                },
-            }
-        })
-    }
-        //game.CreateMaterial(Color.Orange)
-    };
-
-    circleModel = new Model
-    {
-        new Mesh {
-            Draw = GiveMeCircle(boxSize, 10, Color.DarkRed).ToMeshDraw(game.GraphicsDevice),
-            //MaterialIndex = 0
-        },
-        new MaterialInstance
-    {
-        Material = Material.New(game.GraphicsDevice, new MaterialDescriptor
-        {
-            Attributes = new MaterialAttributes
-            {
-                DiffuseModel = new MaterialDiffuseLambertModelFeature(),
-                Diffuse = new MaterialDiffuseMapFeature()
-                {
-                    DiffuseMap = new ComputeVertexStreamColor()
-                },
-            }
-        })
-    }
-    };
+    CreateShapeModels();
 
     //var gameSettings = game.Services.GetService<IGameSettingsService>();
 
     simulation = game.SceneSystem.SceneInstance.GetProcessor<PhysicsProcessor>()?.Simulation;
-
     //var processor = game.SceneSystem.SceneInstance.GetProcessor<PhysicsProcessor>();
 
     simulation.FixedTimeStep = 1f / 120;
     //simulation.ContinuousCollisionDetection = true;
 
-    Add2DShapes(ShapeType.Square, squareModel, boxSize, 1);
+    Add2DShapes(ShapeType.Square, 1);
 
     AddBackground();
 
     //Add3DBoxes(rootScene);
 }
 
+void CreateShapeModels()
+{
+    foreach (var item in shapes)
+    {
+        item.Model =
+        [
+            new Mesh
+            {
+                Draw = CreateMeshDraw(item).ToMeshDraw(game.GraphicsDevice),
+                MaterialIndex = 0
+            },
+            CreateMaterial(game, item.Color)
+        ];
+    }
+}
+
+MeshBuilder CreateMeshDraw(ShapeModel model)
+{
+    return model.Type switch
+    {
+        ShapeType.Square or ShapeType.Rectangle => GiveMeShape(model.Size, model.Color),
+        ShapeType.Circle => GiveMeCircle(model.Size, 10, model.Color),
+        ShapeType.Triangle => GiveMeShape(model.Size, model.Color),
+        _ => GiveMeShape(model.Size, model.Color),
+    };
+}
+
 void Update(Scene scene, GameTime time)
 {
     //var gameSettings = game.Services.GetService<IGameSettingsService>();
-
     //simulation.ContinuousCollisionDetection = true;
 
     if (!game.Input.HasKeyboard) return;
@@ -163,27 +122,27 @@ void Update(Scene scene, GameTime time)
 
     if (game.Input.IsKeyPressed(Keys.N))
     {
-        Add3DBoxes(5);
+        Add3DBoxes(10);
 
-        SetSubeCount(scene);
+        SetCubeCount(scene);
     }
     else if (game.Input.IsKeyPressed(Keys.M))
     {
-        Add2DShapes(ShapeType.Square, squareModel, boxSize, 10);
+        Add2DShapes(ShapeType.Square, 10);
 
-        SetSubeCount(scene);
+        SetCubeCount(scene);
     }
     else if (game.Input.IsKeyPressed(Keys.R))
     {
-        Add2DShapes(ShapeType.Rectangle, rectangleModel, rectangleSize, 10);
+        Add2DShapes(ShapeType.Rectangle, 10);
 
-        SetSubeCount(scene);
+        SetCubeCount(scene);
     }
     else if (game.Input.IsKeyPressed(Keys.C))
     {
-        Add2DShapes(ShapeType.Circle, circleModel, rectangleSize, 10);
+        Add2DShapes(ShapeType.Circle, 10);
 
-        SetSubeCount(scene);
+        SetCubeCount(scene);
     }
     else if (game.Input.IsKeyReleased(Keys.X))
     {
@@ -192,7 +151,7 @@ void Update(Scene scene, GameTime time)
             entity.Remove();
         }
 
-        SetSubeCount(scene);
+        SetCubeCount(scene);
     }
 
     RenderNavigation();
@@ -200,20 +159,20 @@ void Update(Scene scene, GameTime time)
 
 void ProcessRaycast(MouseButton mouseButton, Vector2 mousePosition)
 {
-    var hitResult = _camera!.RaycastMouse(simulation, mousePosition);
+    var hitResult = _camera!.RaycastMouse(simulation!, mousePosition);
 
     if (hitResult.Succeeded && hitResult.Collider.Entity.Name == "Cube")
     {
         if (mouseButton == MouseButton.Left)
         {
-            var rigidbody = hitResult.Collider.Entity.Get<RigidbodyComponent>();
+            var rigidBody = hitResult.Collider.Entity.Get<RigidbodyComponent>();
 
-            if (rigidbody != null)
+            if (rigidBody != null)
             {
                 //var worldPosition = _camera.ScreenToWorldPoint(new Vector3(mousePosition.X, mousePosition.Y, 0));
 
                 // Calculate a target position and apply force or set velocity
-                //var direction = worldPosition - rigidbody.Position;
+                //var direction = worldPosition - rigidBody.Position;
                 //direction.Normalize();
 
                 // Apply a force towards the target position
@@ -221,9 +180,9 @@ void ProcessRaycast(MouseButton mouseButton, Vector2 mousePosition)
 
                 var direction = new Vector3(0, 20, 0);
 
-                rigidbody.ApplyImpulse(direction * 10);
+                rigidBody.ApplyImpulse(direction * 10);
                 // or
-                rigidbody.LinearVelocity = direction * 1;
+                rigidBody.LinearVelocity = direction * 1;
             }
 
 
@@ -231,18 +190,6 @@ void ProcessRaycast(MouseButton mouseButton, Vector2 mousePosition)
         }
     }
 }
-
-//void MoveSelectedEntity(Vector2 mousePosition)
-//{
-//    // Convert mouse position to world coordinates
-//    var worldPosition = ConvertMouseToWorldPosition(mousePosition);
-
-//    // Update entity position
-//    if (_selectedEntity != null)
-//    {
-//        _selectedEntity.Transform.Position = new Vector3(worldPosition.X, worldPosition.Y, _selectedEntity.Transform.Position.Z);
-//    }
-//}
 
 MeshBuilder GiveMeShape(Vector3 size, Color shapeColor)
 {
@@ -368,20 +315,25 @@ void Add3DBoxes(int count = 5)
     }
 }
 
-void Add2DShapes(ShapeType type, Model model, Vector3 size, int count = 5)
+void Add2DShapes(ShapeType type, int count = 5)
 {
+    var shapeModel = shapes.FirstOrDefault(x => x.Type == type);
+
+    if (shapeModel == null) return;
+
     for (int i = 1; i <= count; i++)
     {
         var entity = new Entity
         {
+            Scene = scene,
             Name = "Cube",
             Transform = {
-                Position = new(Random.Shared.Next(-5, 5), 5 + Random.Shared.Next(0, 5), 0),
+                Position = new(Random.Shared.Next(-5, 5), 3 + Random.Shared.Next(0, 7), 0),
                 //Rotation = Quaternion.RotationYawPitchRoll(MathUtil.DegreesToRadians(180), 0, 0)
             }
         };
 
-        entity.Add(new ModelComponent { Model = model });
+        entity.Add(new ModelComponent { Model = shapeModel.Model });
 
         //entity.Add(new StaticColliderComponent());
 
@@ -397,16 +349,14 @@ void Add2DShapes(ShapeType type, Model model, Vector3 size, int count = 5)
             //ColliderShapes = { new BoxColliderShapeDesc() { Size = new Vector3(1), Is2D = true } },
             ColliderShape = (type) switch
             {
-                ShapeType.Square => GetBoxColliderShape(size),
-                ShapeType.Rectangle => GetBoxColliderShape(size),
-                ShapeType.Circle => new SphereColliderShape(true, size.X / 2),
+                ShapeType.Square => GetBoxColliderShape(shapeModel.Size),
+                ShapeType.Rectangle => GetBoxColliderShape(shapeModel.Size),
+                ShapeType.Circle => new SphereColliderShape(true, shapeModel.Size.X / 2),
                 _ => throw new NotImplementedException(),
             }
         };
 
         entity.Add(rigidBody);
-
-        entity.Scene = scene;
 
         rigidBody.AngularFactor = new Vector3(0, 0, 1);
         rigidBody.LinearFactor = new Vector3(1, 1, 0);
@@ -427,23 +377,61 @@ static void AddSpriteBatchRenderer(Scene rootScene)
 void RenderNavigation()
 {
     game.DebugTextSystem.Print($"Cubes: {cubes}", new Int2(x: debugX, y: debugY));
-    game.DebugTextSystem.Print($"X - delete all cubes and shapes", new Int2(x: debugX, y: debugY + 30));
-    game.DebugTextSystem.Print($"N - generate 3D cubes", new Int2(x: debugX, y: debugY + 50));
-    game.DebugTextSystem.Print($"M - generate 2D squares", new Int2(x: debugX, y: debugY + 70));
+    game.DebugTextSystem.Print($"X - Delete all cubes and shapes", new Int2(x: debugX, y: debugY + 30), Color.Red);
+    game.DebugTextSystem.Print($"N - generate 3D cubes", new Int2(x: debugX, y: debugY + 60));
+    game.DebugTextSystem.Print($"M - generate 2D squares", new Int2(x: debugX, y: debugY + 80));
+    game.DebugTextSystem.Print($"C - generate 2D circles", new Int2(x: debugX, y: debugY + 100));
+    game.DebugTextSystem.Print($"R - generate 2D rectangles", new Int2(x: debugX, y: debugY + 120));
+    game.DebugTextSystem.Print($"T - generate random 2D shapes", new Int2(x: debugX, y: debugY + 140));
 }
 
-void SetSubeCount(Scene scene) => cubes = scene.Entities.Where(w => w.Name == "Cube").Count();
+void SetCubeCount(Scene scene) => cubes = scene.Entities.Where(w => w.Name == "Cube").Count();
 
 static BoxColliderShapeX4 GetBoxColliderShape(Vector3 size)
-    => new BoxColliderShapeX4(true, new Vector3(size.X, size.Y, 0))
+    => new(true, new Vector3(size.X, size.Y, 0))
     {
         LocalOffset = new Vector3(size.X / 2, size.Y / 2, 0)
     };
 
-enum ShapeType
+static Material CreateMaterial(Game game, Color color)
+{
+    var colorVertexStream = new ComputeVertexStreamColor { Stream = new ColorVertexStreamDefinition() };
+    var computeColor = new ComputeBinaryColor(new ComputeColor(color), colorVertexStream, BinaryOperator.Multiply);
+
+    return Material.New(game.GraphicsDevice, new MaterialDescriptor
+    {
+        Attributes = new MaterialAttributes
+        {
+            // Best colours
+            Diffuse = new MaterialDiffuseMapFeature
+            {
+                DiffuseMap = new ComputeVertexStreamColor()
+            },
+            // Better colours
+            //Diffuse = new MaterialDiffuseMapFeature(new ComputeColor(color)),
+            // Worst colours
+            //Diffuse = new MaterialDiffuseMapFeature(computeColor),
+            DiffuseModel = new MaterialDiffuseLambertModelFeature(),
+            Specular = new MaterialMetalnessMapFeature(new ComputeFloat(0)),
+            SpecularModel = new MaterialSpecularMicrofacetModelFeature(),
+            MicroSurface = new MaterialGlossinessMapFeature(new ComputeFloat(0.05f)),
+            Emissive = new MaterialEmissiveMapFeature(computeColor),
+        }
+    });
+}
+
+public enum ShapeType
 {
     Square,
     Rectangle,
     Circle,
     Triangle
+}
+
+public class ShapeModel
+{
+    public required ShapeType Type { get; set; }
+    public required Color Color { get; set; }
+    public required Vector3 Size { get; set; }
+    public Model? Model { get; set; }
 }
