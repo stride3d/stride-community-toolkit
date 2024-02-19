451e944c56ac102872ff18c7ffee68547b061e02
Doprez <73259914+Doprez@users.noreply.github.com>
2024-02-19 10:44:08 -0700

    made old method obsolete

--- src/Stride.CommunityToolkit/Engine/ScriptComponentExtensions.cs
+++ src/Stride.CommunityToolkit/Engine/ScriptComponentExtensions.cs
@@ -23,7 +23,31 @@ public static class ScriptComponentExtensions
     /// </remarks>
     /// <param name="scriptComponent">The script component from which to access the GraphicsCompositor.</param>
     /// <returns>The <see cref="CameraComponent"/> named "Main", if found; otherwise, null.</returns>
+    [Obsolete("Use GetGCCamera instead")]
     public static CameraComponent? GetCamera(this ScriptComponent scriptComponent)
+    {
+        var cameraCollection = scriptComponent.SceneSystem.GraphicsCompositor.Cameras;
+
+        foreach (var sceneCamera in cameraCollection)
+        {
+            if (sceneCamera.Name == "Main")
+            {
+                return sceneCamera.Camera;
+            }
+        }
+
+        return null;
+    }
+
+    /// <summary>
+    /// Retrieves the camera named "Main" from the <see cref="GraphicsCompositor"/>. Note that the camera might not be available during the first 2-3 frames.
+    /// </summary>
+    /// <remarks>
+    /// Ensure that the GraphicsCompositor is initialized with cameras; otherwise, this method will fail.
+    /// </remarks>
+    /// <param name="scriptComponent">The script component from which to access the GraphicsCompositor.</param>
+    /// <returns>The <see cref="CameraComponent"/> named "Main", if found; otherwise, null.</returns>
+    public static CameraComponent? GetGCCamera(this ScriptComponent scriptComponent)
     {
         var cameraCollection = scriptComponent.SceneSystem.GraphicsCompositor.Cameras;
 
@@ -47,7 +71,32 @@ public static class ScriptComponentExtensions
     /// <param name="scriptComponent">The script component from which to access the GraphicsCompositor.</param>
     /// <param name="cameraName">The name of the camera to retrieve.</param>
     /// <returns>The <see cref="CameraComponent"/> with the given name, if found; otherwise, null.</returns>
+    [Obsolete("Use GetGCCamera instead")]
     public static CameraComponent? GetCamera(this ScriptComponent scriptComponent, string cameraName)
+    {
+        var cameraCollection = scriptComponent.SceneSystem.GraphicsCompositor.Cameras;
+
+        foreach (var sceneCamera in cameraCollection)
+        {
+            if (sceneCamera.Name == cameraName)
+            {
+                return sceneCamera.Camera;
+            }
+        }
+
+        return null;
+    }
+
+    /// <summary>
+    /// Retrieves the camera from the <see cref="GraphicsCompositor"/> with the specified name. Note that the camera might not be available during the first 2-3 frames.
+    /// </summary>
+    /// <remarks>
+    /// Ensure that the GraphicsCompositor is initialized with cameras; otherwise, this method will fail.
+    /// </remarks>
+    /// <param name="scriptComponent">The script component from which to access the GraphicsCompositor.</param>
+    /// <param name="cameraName">The name of the camera to retrieve.</param>
+    /// <returns>The <see cref="CameraComponent"/> with the given name, if found; otherwise, null.</returns>
+    public static CameraComponent? GetGCCamera(this ScriptComponent scriptComponent, string cameraName)
     {
         var cameraCollection = scriptComponent.SceneSystem.GraphicsCompositor.Cameras;
 
@@ -70,7 +119,23 @@ public static class ScriptComponentExtensions
     /// </remarks>
     /// <param name="scriptComponent">The script component from which to access the GraphicsCompositor.</param>
     /// <returns>The <see cref="CameraComponent"/> with the given name, if found; otherwise, null.</returns>
+    [Obsolete("Use GetFirstGCCamera instead")]
     public static CameraComponent GetFirstCamera(this ScriptComponent scriptComponent)
+    {
+        SceneCameraSlotCollection cameraCollection = scriptComponent.SceneSystem.GraphicsCompositor.Cameras;
+
+        return cameraCollection[0].Camera;
+    }
+
+    /// <summary>
+    /// Gets the first camera from the <see cref="GraphicsCompositor"/>. Note that the camera might not be available during the first 2-3 frames.
+    /// </summary>
+    /// <remarks>
+    /// Ensure that the GraphicsCompositor is initialized with cameras; otherwise, this method will fail.
+    /// </remarks>
+    /// <param name="scriptComponent">The script component from which to access the GraphicsCompositor.</param>
+    /// <returns>The <see cref="CameraComponent"/> with the given name, if found; otherwise, null.</returns>
+    public static CameraComponent GetFirstGCCamera(this ScriptComponent scriptComponent)
     {
         SceneCameraSlotCollection cameraCollection = scriptComponent.SceneSystem.GraphicsCompositor.Cameras;
 
