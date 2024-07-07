cf0a4930c0500406905d619598aa7da7e7919fed
Doprez <73259914+Doprez@users.noreply.github.com>
2024-07-07 13:58:05 -0600

    docs

--- src/Stride.CommunityToolkit/Games/GameExtensions.cs
+++ src/Stride.CommunityToolkit/Games/GameExtensions.cs
@@ -55,11 +55,19 @@ public static class GameExtensions
         gameBase.WindowMinimumUpdateRate.MinimumElapsedTime = TimeSpan.FromMilliseconds(1000f / targetFPS);
     }
 
+    /// <summary>
+    /// Enables vertical synchronization (VSync) to prevent screen tearing.
+    /// </summary>
+    /// <param name="game"></param>
     public static void EnableVSync(this IGame game)
     {
         game.GraphicsDevice.Presenter.PresentInterval = Stride.Graphics.PresentInterval.Two;
     }
 
+    /// <summary>
+    /// Disables vertical synchronization (VSync) to allow for uncapped frame rates.
+    /// </summary>
+    /// <param name="game"></param>
     public static void DisableVSync(this IGame game)
     {
         game.GraphicsDevice.Presenter.PresentInterval = Stride.Graphics.PresentInterval.Immediate;
