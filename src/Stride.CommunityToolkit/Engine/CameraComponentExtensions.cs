3d0ae2aeeef938ae5cbb17084e4699277c185028
Vaclav Elias <vaclavelias@gmail.com>
2024-08-25 23:53:18 +0100

    feat: XML comments improvements

--- src/Stride.CommunityToolkit/Engine/CameraComponentExtensions.cs
+++ src/Stride.CommunityToolkit/Engine/CameraComponentExtensions.cs
@@ -133,33 +133,44 @@ public static class CameraComponentExtensions
     public static HitResult Raycast(this CameraComponent camera, ScriptComponent component, Vector2 screenPosition, CollisionFilterGroups collisionGroups = CollisionFilterGroups.DefaultFilter, CollisionFilterGroupFlags collisionFilterGroupFlags = CollisionFilterGroupFlags.DefaultFilter)
         => Raycast(camera, component.GetSimulation(), screenPosition, collisionGroups, collisionFilterGroupFlags);
 
+    /// <summary>
+    /// Performs a raycasting operation from the specified <see cref="CameraComponent"/>'s position through a specified screen position,
+    /// using the provided <see cref="Simulation"/>, and returns information about the hit result.
+    /// </summary>
+    /// <param name="camera">The <see cref="CameraComponent"/> from which the ray should be cast.</param>
+    /// <param name="simulation">The <see cref="Simulation"/> used to perform the raycasting operation.</param>
+    /// <param name="screenPosition">The screen position in normalized screen coordinates (e.g., mouse position) where the ray should be cast.</param>
+    /// <param name="collisionGroups">Optional. The collision filter group to consider during the raycasting. Default is <see cref="CollisionFilterGroups.DefaultFilter"/>.</param>
+    /// <param name="collisionFilterGroupFlags">Optional. The collision filter group flags to consider during the raycasting. Default is <see cref="CollisionFilterGroupFlags.DefaultFilter"/>.</param>
+    /// <returns>A <see cref="HitResult"/> containing information about the raycasting hit, including the hit location and other collision data.</returns>
+    /// <remarks>
+    /// This method is useful for implementing features like object picking, where you want to select or interact with objects in the 3D world based on screen coordinates.
+    /// </remarks>///
     public static HitResult Raycast(this CameraComponent camera, Simulation simulation, Vector2 screenPosition, CollisionFilterGroups collisionGroups = CollisionFilterGroups.DefaultFilter, CollisionFilterGroupFlags collisionFilterGroupFlags = CollisionFilterGroupFlags.DefaultFilter)
     {
+        // Invert the view-projection matrix to transform from screen space to world space
         var invertedMatrix = Matrix.Invert(camera.ViewProjectionMatrix);
 
         // Reconstruct the projection-space position in the (-1, +1) range.
-        //    Don't forget that Y is down in screen coordinates, but up in projection space
+        // The X coordinate maps directly from screen space (0,1) to projection space (-1,+1).
+        // The Y coordinate is inverted because screen space is Y-down, while projection space is Y-up.
         Vector3 position;
         position.X = screenPosition.X * 2f - 1f;
         position.Y = 1f - screenPosition.Y * 2f;
 
-        // Compute the near (start) point for the raycast
-        // It's assumed to have the same projection space (x,y) coordinates and z = 0 (lying on the near plane)
-        // We need to unproject it to world space
+        // Set Z = 0 for the near plane (the starting point of the ray)
+        // Unproject the near point from projection space to world space
         position.Z = 0f;
-
         var vectorNear = Vector3.Transform(position, invertedMatrix);
-        vectorNear /= vectorNear.W;
+        vectorNear /= vectorNear.W; // Normalize by the homogeneous W component
 
-        // Compute the far (end) point for the raycast
-        // It's assumed to have the same projection space (x,y) coordinates and z = 1 (lying on the far plane)
-        // We need to unproject it to world space
+        // Set Z = 1 for the far plane (the end point of the ray)
+        // Unproject the far point from projection space to world space
         position.Z = 1f;
-
         var vectorFar = Vector3.Transform(position, invertedMatrix);
         vectorFar /= vectorFar.W;
 
-        // Raycast from the point on the near plane to the point on the far plane and get the collision result
+        // Perform the raycast from the near point to the far point and return the result
         return simulation.Raycast(vectorNear.XYZ(), vectorFar.XYZ(), collisionGroups, collisionFilterGroupFlags);
     }
 
