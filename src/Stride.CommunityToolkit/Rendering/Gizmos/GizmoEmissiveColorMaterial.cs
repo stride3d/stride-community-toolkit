361c2da3cdafeb4afa95b9a6208382d2cc2b5d1b
Vaclav Elias <vaclavelias@gmail.com>
2024-09-14 00:15:16 +0100

    docs: XML comments updates

--- src/Stride.CommunityToolkit/Rendering/Gizmos/GizmoEmissiveColorMaterial.cs
+++ src/Stride.CommunityToolkit/Rendering/Gizmos/GizmoEmissiveColorMaterial.cs
@@ -5,8 +5,18 @@ using Stride.Rendering.Materials.ComputeColors;
 
 namespace Stride.CommunityToolkit.Rendering.Gizmos;
 
+/// <summary>
+/// A utility class for creating and updating materials with emissive color properties for gizmos.
+/// </summary>
 public static class GizmoEmissiveColorMaterial
 {
+    /// <summary>
+    /// Creates a new material with emissive color and diffuse properties based on the specified color and intensity.
+    /// </summary>
+    /// <param name="device">The <see cref="GraphicsDevice"/> used to create the material.</param>
+    /// <param name="color">The <see cref="Color"/> to apply to the material.</param>
+    /// <param name="intensity">The intensity of the emissive color. Defaults to 1f.</param>
+    /// <returns>A new <see cref="Material"/> with the specified emissive color and intensity.</returns>
     public static Material Create(GraphicsDevice device, Color color, float intensity = 1f)
     {
         var material = Material.New(device, new MaterialDescriptor
@@ -19,10 +29,10 @@ public static class GizmoEmissiveColorMaterial
                 }
         });
 
-        // set the color to the material
+        // Set the color to the material
         UpdateColor(device, material, color, intensity);
 
-        // set the transparency property to the material if necessary
+        // Set the transparency property to the material if necessary
         if (color.A < byte.MaxValue)
         {
             material.Passes[0].HasTransparency = true;
@@ -31,43 +41,17 @@ public static class GizmoEmissiveColorMaterial
         return material;
     }
 
-    public static void UpdateColor(GraphicsDevice device, Material material, Color color, float intensity = 1f)
+    /// <summary>
+    /// Updates the color and emissive properties of an existing material.
+    /// </summary>
+    /// <param name="device">The <see cref="GraphicsDevice"/> used to update the color.</param>
+    /// <param name="material">The <see cref="Material"/> to be updated.</param>
+    /// <param name="color">The <see cref="Color"/> to apply to the material.</param>
+    /// <param name="intensity">The intensity of the emissive color. Defaults to 1f.</param>
+    private static void UpdateColor(GraphicsDevice device, Material material, Color color, float intensity = 1f)
     {
-        // set the color to the material
         material.Passes[0].Parameters.Set(MaterialKeys.DiffuseValue, new Color4(color).ToColorSpace(device.ColorSpace));
         material.Passes[0].Parameters.Set(MaterialKeys.EmissiveIntensity, intensity);
         material.Passes[0].Parameters.Set(MaterialKeys.EmissiveValue, new Color4(color).ToColorSpace(device.ColorSpace));
-    }
-}
-
-public static class GizmoUniformColorMaterial
-{
-    public static Material Create(GraphicsDevice device, Color color)
-    {
-        var descriptor = new MaterialDescriptor();
-        descriptor.Attributes.Diffuse = new MaterialDiffuseMapFeature(new ComputeColor());
-        descriptor.Attributes.DiffuseModel = new MaterialDiffuseLambertModelFeature();
-
-        var material = Material.New(device, descriptor);
-
-        // set the color to the material
-        UpdateColor(device, material, color);
-
-        // set the transparency property to the material if necessary
-        if (color.A < Byte.MaxValue)
-        {
-
-            material.Passes[0].HasTransparency = true;
-            // TODO GRAPHICS REFACTOR
-            //material.Parameters.SetResourceSlow(Graphics.Effect.BlendStateKey, device.BlendStates.NonPremultiplied);
-        }
-
-        return material;
-    }
-
-    public static void UpdateColor(GraphicsDevice device, Material material, Color color)
-    {
-        // set the color to the material
-        material.Passes[0].Parameters.Set(MaterialKeys.DiffuseValue, new Color4(color).ToColorSpace(device.ColorSpace));
     }
 }
