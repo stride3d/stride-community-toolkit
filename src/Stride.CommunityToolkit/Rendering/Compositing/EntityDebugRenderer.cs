246b94c1e2d55431c1eadc4f6ce8e44a3db6e7c9
Vaclav Elias <vaclavelias@gmail.com>
2024-10-03 00:35:20 +0100

    feat: EntityDebugRenderer added

---
+++ src/Stride.CommunityToolkit/Rendering/Compositing/EntityDebugRenderer.cs
@@ -0,0 +1,9 @@
+using Stride.Rendering;
+using Stride.Rendering.Compositing;
+
+namespace Stride.CommunityToolkit.Rendering.Compositing;
+
+public class EntityDebugRenderer : SceneRendererBase
+{
+    protected override void DrawCore(RenderContext context, RenderDrawContext drawContext) => throw new NotImplementedException();
+}
