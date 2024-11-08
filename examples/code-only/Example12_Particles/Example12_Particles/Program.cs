c6eaacb36cc7359af0f1b2d529a618d8ef286309
Doprez <73259914+Doprez@users.noreply.github.com>
2024-11-08 08:21:28 -0700

    add particle example

---
+++ examples/code-only/Example12_Particles/Example12_Particles/Program.cs
@@ -0,0 +1,127 @@
+using Stride.Animations;
+using Stride.CommunityToolkit.Engine;
+using Stride.CommunityToolkit.Games;
+using Stride.CommunityToolkit.ImGui;
+using Stride.CommunityToolkit.Skyboxes;
+using Stride.Core.Mathematics;
+using Stride.Engine;
+using Stride.Particles;
+using Stride.Particles.Components;
+using Stride.Particles.Initializers;
+using Stride.Particles.Materials;
+using Stride.Particles.Modules;
+using Stride.Particles.ShapeBuilders;
+using Stride.Particles.Spawners;
+using Stride.Rendering.Materials.ComputeColors;
+
+using var game = new Game();
+
+game.Run(start: Start);
+
+void Start(Scene scene)
+{
+    SetupBaseScene();
+
+    // Add debugging aids: entity names, positions
+    //game.AddEntityDebugSceneRenderer(new()
+    //{
+    //    ShowFontBackground = true
+    //});
+
+    game.AddSkybox();
+    game.AddProfiler();
+
+    // makes the profiling much easier to read.
+    game.SetMaxFPS(60);
+
+    CreateParticleEffect();
+}
+
+void SetupBaseScene()
+{
+    game.AddGraphicsCompositor();
+    game.Add3DCamera().Add3DCameraController();
+    game.AddDirectionalLight();
+    game.AddSkybox();
+    game.Add3DGround();
+    game.AddParticleRenderer();
+}
+
+void CreateParticleEffect()
+{
+
+    var emitter = new ParticleEmitter
+    {
+        ParticleLifetime = new Vector2(0.5f, 0.5f),
+        SimulationSpace = EmitterSimulationSpace.World,
+        RandomSeedMethod = EmitterRandomSeedMethod.Time,
+        ShapeBuilder = new ShapeBuilderBillboard(),
+
+        Material = new ParticleMaterialComputeColor()
+        {
+            ComputeColor = new ComputeColor()
+            {
+                Value = new Color4(0, 0, 1, 1)
+            }
+        },
+        Enabled = true,
+    };
+
+    emitter.Spawners.Add(new SpawnerPerSecond()
+    {
+        LoopCondition = SpawnerLoopCondition.Looping,
+        Delay = new Vector2(),
+        Duration = new Vector2(1, 1),
+        SpawnCount = 50,
+        Enabled = true,
+    });
+
+    var sizeInitializer = new InitialSizeSeed()
+    {
+        ScaleUniform = 0.3f,
+        RandomSize = new Vector2(0.1f, 0.5f),
+    };
+
+    var positionInitializer = new InitialPositionSeed()
+    {
+        PositionMin = new Vector3(-0.03f, -0.03f, -0.03f),
+        PositionMax = new Vector3(0.03f, 0.03f, 0.03f),
+    };
+
+    var velocityInitialzer = new InitialVelocitySeed()
+    {
+        VelocityMin = new Vector3(0, 3, 0),
+        VelocityMax = new Vector3(3, 4, 3),
+    };
+
+    emitter.Initializers.Add(sizeInitializer);
+    emitter.Initializers.Add(positionInitializer);
+    emitter.Initializers.Add(velocityInitialzer);
+
+    emitter.Updaters.Add(new UpdaterGravity() { GravitationalAcceleration = new Vector3(0, -9.8f, 0) });
+
+    var particleSettings = new ParticleSystemSettings
+    {
+        WarmupTime = 0,
+    };
+
+    ParticleSystemComponent particles = new()
+    {
+        Color = Color.White,
+        RenderGroup = Stride.Rendering.RenderGroup.Group0,
+        Speed = 1,
+        Enabled = true,
+    };
+    particles.ParticleSystem.Emitters.Add(emitter);
+    particles.ParticleSystem.Settings = particleSettings;
+
+    var entity = new Entity
+    {
+        particles
+    };
+    entity.Name = "Particles";
+    entity.Scene = game.SceneSystem.SceneInstance.RootScene;
+    entity.Transform.Position = new Vector3(0, 0, 0);
+
+    particles.Enabled = true;
+}
