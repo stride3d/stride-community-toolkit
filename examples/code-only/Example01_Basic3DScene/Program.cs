f6dcca44a46433fd9a3f5d1b0a6342535bc10382
Vaclav Elias <vaclavelias@gmail.com>
2024-12-24 19:54:35 +0000

    refactor: Examples corrections

--- examples/code-only/Example01_Basic3DScene/Program.cs
+++ examples/code-only/Example01_Basic3DScene/Program.cs
@@ -1,4 +1,5 @@
-using Stride.CommunityToolkit.Bullet;
+using Stride.BepuPhysics;
+using Stride.CommunityToolkit.Bepu;
 using Stride.CommunityToolkit.Engine;
 using Stride.CommunityToolkit.Rendering.ProceduralModels;
 using Stride.CommunityToolkit.Skyboxes;
@@ -12,9 +13,25 @@ game.Run(start: (Scene rootScene) =>
     game.SetupBase3DScene();
     game.AddSkybox();
 
-    var entity = game.Create3DPrimitive(PrimitiveModelType.Capsule);
+    var type = PrimitiveModelType.Capsule;
+
+    var entity = game.Create3DPrimitive(type);
 
     entity.Transform.Position = new Vector3(0, 8, 0);
 
+    var body = entity.Get<BodyComponent>();
+    body.Awake = true;
+
     entity.Scene = rootScene;
+
+    var entity2 = game.Create3DPrimitive(type);
+
+    var body2 = entity2.Get<BodyComponent>();
+    body2.Awake = true;
+
+    entity2.Transform.Position = new Vector3(0, 20, 0);
+
+    entity2.Scene = rootScene;
+
+    body.ApplyLinearImpulse(new Vector3(0, 0, 0.001f));
 });
