0614468695ebe75244c0f3ddaa8477fdff451e1b
Vaclav Elias <vaclavelias@gmail.com>
2025-01-04 18:46:27 +0000

    feat: Bullet example added

---
+++ examples/code-only/Example01_Basic3DScene_BulletPhysics/Program.cs
@@ -0,0 +1,20 @@
+using Stride.CommunityToolkit.Bullet;
+using Stride.CommunityToolkit.Engine;
+using Stride.CommunityToolkit.Rendering.ProceduralModels;
+using Stride.CommunityToolkit.Skyboxes;
+using Stride.Core.Mathematics;
+using Stride.Engine;
+
+using var game = new Game();
+
+game.Run(start: (Scene rootScene) =>
+{
+    game.SetupBase3DScene();
+    game.AddSkybox();
+
+    var entity = game.Create3DPrimitive(PrimitiveModelType.Capsule);
+
+    entity.Transform.Position = new Vector3(0, 8, 0);
+
+    entity.Scene = rootScene;
+});
