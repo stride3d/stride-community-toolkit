d13e62080f2ceaa24fd7bf5703439d7c6cc13d2a
Vaclav Elias <vaclavelias@gmail.com>
2025-01-18 19:59:22 +0000

    fix: Upadting command line to avoid IOException error

--- docs/run.bat
+++ docs/run.bat
@@ -1,1 +1,12 @@
+REM Delete all .yml files in the api directory
+del /Q /S "api\*.yml"
+
+REM Delete all .yml files in the api directory
+REM del /Q /S "site\api\*.*"
+
+REM Delete the .manifest file in the api directory
+del /Q "api\.manifest"
+
+REM --maxParallelism 1
+
 docfx docfx.json --serve
