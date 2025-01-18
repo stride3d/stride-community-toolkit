2cf5da2801c0257910ae8b8d5f59fef50b59cc1b
Vaclav Elias <vaclavelias@gmail.com>
2025-01-18 22:19:27 +0000

    feat: Delete manual temp files added

--- docs/run.bat
+++ docs/run.bat
@@ -2,11 +2,12 @@ REM Delete all .yml files in the api directory
 del /Q /S "api\*.yml"
 
 REM Delete all .yml files in the api directory
-REM del /Q /S "site\api\*.*"
+del /Q /S "_site\api\*.*"
+del /Q /S "_site\manual\*.*"
 
 REM Delete the .manifest file in the api directory
 del /Q "api\.manifest"
 
 REM --maxParallelism 1
 
-docfx docfx.json --serve
+docfx docfx.json --serve --maxParallelism 1
