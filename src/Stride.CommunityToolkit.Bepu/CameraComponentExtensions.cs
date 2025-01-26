6f68106355e83ffbc1477a388a9c8c9b88be38d2
Vaclav Elias <vaclavelias@gmail.com>
2025-01-26 02:27:04 +0000

    refactor: Rename and update Raycast methods
    - Renamed Raycast() in CameraComponentExtensions to RaycastMouse() and added ScriptComponent component parameter.
    - Modified original Raycast() to include a maxDistance parameter with a default value of 100

--- src/Stride.CommunityToolkit.Bepu/CameraComponentExtensions.cs
+++ src/Stride.CommunityToolkit.Bepu/CameraComponentExtensions.cs
@@ -7,15 +7,14 @@ namespace Stride.CommunityToolkit.Bepu;
 
 public static class CameraComponentExtensions
 {
-    public static HitInfo? Raycast(this CameraComponent camera, Vector2 screenPosition, CollisionMask collisionMask = CollisionMask.Everything)
+    public static HitInfo? RaycastMouse(this CameraComponent camera, ScriptComponent component, float maxDistance = 100, CollisionMask collisionMask = CollisionMask.Everything)
+        => camera.Raycast(component.Input.MousePosition, maxDistance, collisionMask);
+
+    public static HitInfo? Raycast(this CameraComponent camera, Vector2 screenPosition, float maxDistance = 100, CollisionMask collisionMask = CollisionMask.Everything)
     {
         var (nearPoint, farPoint) = camera.CalculateRayFromScreenPosition(screenPosition);
 
-        //var delta = (farPoint - nearPoint).XYZ();
-        //var maxDistance = delta.Length();
-        //var dir = delta / maxDistance; // normalize delta
-
-        var result = camera.Entity.GetSimulation().RayCast(nearPoint, farPoint, 100, out HitInfo hit, collisionMask);
+        var result = camera.Entity.GetSimulation().RayCast(nearPoint, farPoint, maxDistance, out HitInfo hit, collisionMask);
 
         if (!result) return null;
 
