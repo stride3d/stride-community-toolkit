fba9078e773d31b0e789400e0449ca073730f126
Vaclav Elias <vaclavelias@gmail.com>
2025-02-02 02:21:52 +0000

    feat: Instructions added and random direction added

--- examples/code-only/Example14_Raycast/Program.cs
+++ examples/code-only/Example14_Raycast/Program.cs
@@ -1,6 +1,7 @@
 using Stride.BepuPhysics;
 using Stride.CommunityToolkit.Bepu;
 using Stride.CommunityToolkit.Engine;
+using Stride.CommunityToolkit.Helpers;
 using Stride.CommunityToolkit.Rendering.ProceduralModels;
 using Stride.CommunityToolkit.Skyboxes;
 using Stride.Core.Mathematics;
@@ -20,7 +21,7 @@ void Start(Scene scene)
     game.AddSkybox();
     game.AddGroundGizmo(new(-5, 0, -5), showAxisName: true);
 
-    var entity = game.Create3DPrimitive(PrimitiveModelType.Capsule);
+    var entity = game.Create3DPrimitive(PrimitiveModelType.Sphere);
 
     entity.Transform.Position = new Vector3(0, 8, 0);
 
@@ -33,21 +34,24 @@ void Update(Scene scene, GameTime time)
 {
     if (camera == null) return;
 
+    game.DebugTextSystem.Print("Click the sphere to apply a random impulse", new(5, 10));
+
     if (game.Input.IsMouseButtonPressed(MouseButton.Left))
     {
         var hit = camera.Raycast(game.Input.MousePosition, 100, out var hitInfo);
 
         if (hit)
         {
-            Console.WriteLine(hitInfo.Collidable.Entity);
-
             var body = hitInfo.Collidable.Entity.Get<BodyComponent>();
 
             if (body is null) return;
 
-            var direction = new Vector3(1, 1, 1);
+            var randomDirection = VectorHelper.RandomVector3([-1, 1], [0, 1], [-1, 1]);
 
-            body.ApplyImpulse(direction * 1, new());
+            Console.WriteLine($"{hitInfo.Collidable.Entity}, direction: {randomDirection}");
+
+            body.ApplyImpulse(randomDirection * 2, new());
+
             body.Awake = true;
         }
         else
