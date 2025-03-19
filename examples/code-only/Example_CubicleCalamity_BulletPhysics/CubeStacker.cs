4be256ddc5e912fa485fd7e807e22b6e8f9c5fa7
Vaclav Elias <vaclavelias@gmail.com>
2025-03-19 23:25:14 +0000

    refactor: Code readability improvements

--- examples/code-only/Example_CubicleCalamity_BulletPhysics/CubeStacker.cs
+++ examples/code-only/Example_CubicleCalamity_BulletPhysics/CubeStacker.cs
@@ -1,6 +1,7 @@
 using Example_CubicleCalamity.Components;
 using Example_CubicleCalamity.Scripts;
 using Example_CubicleCalamity.Shared;
+using Example_CubicleCalamity_BulletPhysics;
 using Stride.CommunityToolkit.Bullet;
 using Stride.CommunityToolkit.Engine;
 using Stride.CommunityToolkit.Rendering.Compositing;
