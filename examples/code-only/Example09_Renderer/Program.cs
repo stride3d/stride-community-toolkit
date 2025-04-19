76323c1b01513f96f785e2ec35f5e054a1d61103
Vaclav Elias <vaclavelias@gmail.com>
2025-04-19 16:13:00 +0100

    refactor: Code improvements

--- examples/code-only/Example09_Renderer/Program.cs
+++ examples/code-only/Example09_Renderer/Program.cs
@@ -9,10 +9,11 @@ using Stride.Core.Mathematics;
 using Stride.Engine;
 using Stride.Games;
 
-// This example demonstrates two different ways of adding custom rendering logic to a Stride game.
+// This example demonstrates three different ways of adding custom rendering logic to a Stride game.
 // 1. Using a custom SceneRenderer via MyCustomSceneRenderer, which renders text for all entities.
-// 2. Using a StartupScript via SpriteBatchRendererScript, which draws specific text for a single entity.
-// Both approaches integrate into the Stride rendering pipeline, demonstrating how to extend the default rendering behaviour.
+// 2. Using a custom EntityTextComponent, which is a component that can be added to any entity to render text.
+// 3. Using a StartupScript via SpriteBatchRendererScript, which draws specific text for a single entity.
+// All approaches integrate into the Stride rendering pipeline, demonstrating how to extend the default rendering behaviour.
 
 BodyComponent? body = null;
 bool impluseApplied = false;
@@ -28,60 +29,60 @@ game.Run(start: Start, update: Update);
 
 void Start(Scene scene)
 {
-    // Sets up the base 3D scene, including lighting, camera, and default settings.
+    // Sets up the base 3D scene, including lighting, camera, and default settings
     game.SetupBase3DScene();
 
-    // Adds the built-in profiler, which provides real-time performance metrics.
+    // Adds the built-in profiler, which provides real-time performance metrics
     game.AddProfiler();
 
-    // Example 1: Adds a custom scene renderer to render text for all entities in the scene.
-    game.AddSceneRenderer(new MyCustomSceneRenderer());
-    game.AddSceneRenderer(new EntityTextRenderer());
-
-    // Adds a skybox (a background environment) to the scene.
+    // Adds a skybox (a background environment) to the scene
     game.AddSkybox();
 
-    // Creates a 3D capsule primitive and sets its position in the scene.
-    var entity = game.Create3DPrimitive(PrimitiveModelType.Capsule);
-    entity.Transform.Position = new Vector3(0, 8, 0);
+    // Example 1: Adds a custom scene renderer to render text for all entities in the scene and also "Hello Stride" text
+    game.AddSceneRenderer(new MyCustomSceneRenderer());
 
-    // Let's rotate the capsule a tiny bit
-    entity.Transform.Rotation = Quaternion.RotationZ(MathUtil.DegreesToRadians(2));
+    // Example 2: Adds a custom scene renderer to render text for all entities with a component of type EntityTextComponent
+    game.AddSceneRenderer(new EntityTextRenderer());
+
+    // Creates a 3D primitive (a cube) and sets its position in the scene
+    var cube = game.Create3DPrimitive(PrimitiveModelType.Cube);
+    cube.Transform.Position = new Vector3(0, 0.5f, 0);
+
+    // Example 2: Adds an EntityTextComponent to the cube entity, which renders text
+    cube.Add(new EntityTextComponent()
+    {
+        Text = "Me, Cube",
+        FontSize = 12,
+        TextColor = Color.Purple,
+    });
+
+    cube.Scene = scene;
+
+    // Creates a 3D primitive (a capsule) and sets its position in the scene
+    var capsule = game.Create3DPrimitive(PrimitiveModelType.Capsule);
+    capsule.Transform.Position = new Vector3(0, 8, 0);
+
+    // Lets tilt the capsule a bit which should cause it to move after it falls
+    capsule.Transform.Rotation = Quaternion.RotationZ(MathUtil.DegreesToRadians(2));
 
     // Get the body component
-    body = entity.Get<BodyComponent>();
+    body = capsule.Get<BodyComponent>();
 
-    // Example 2: Adds a custom startup script to the entity, which draws specific text
-    // (e.g., "Hello Stride 2") using SpriteBatch when the game is running.
-
-    entity.Add(new SpriteBatchRendererScript());
+    // Example 3: Adds a custom startup script to the entity, which draws specific text using SpriteBatch when the game is running.
+    capsule.Add(new SpriteBatchRendererScript());
 
     var textComponent = new EntityTextComponent()
     {
-        Text = "Hello Boss",
+        Text = "Hello, Stride!",
         FontSize = 13,
         TextColor = Color.Green,
-        Offset = new(0, -100)
+        Offset = new(0, -100),
+        EnableBackground = true,
     };
-    entity.Add(textComponent);
+    capsule.Add(textComponent);
 
     // Assigns the entity to the root scene to ensure it is rendered and updated.
-    entity.Scene = scene;
-
-    ////////////////
-    ///
-    var entity2 = game.Create3DPrimitive(PrimitiveModelType.Cube);
-    entity2.Transform.Position = new Vector3(0, 3, 0);
-
-    var textComponent2 = new EntityTextComponent()
-    {
-        Text = "Hello Stride 2026",
-        FontSize = 14,
-        TextColor = Color.Red,
-    };
-    entity2.Add(textComponent2);
-
-    entity2.Scene = scene;
+    capsule.Scene = scene;
 }
 
 void Update(Scene scene, GameTime time)
@@ -91,8 +92,8 @@ void Update(Scene scene, GameTime time)
     if (impluseApplied) return;
 
     // Let's add some momentum so it rolls after it falls, the rigid body is already added by Create3DPrimitive
-    body.ApplyImpulse(new(0, 0, 1f), new());
-    body.ApplyAngularImpulse(new(2, 0, 0));
+    //body.ApplyImpulse(new(0, 0, 0.1f), new());
+    body.ApplyAngularImpulse(new(0, 10, 0));
 
     impluseApplied = true;
 }
