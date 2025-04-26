feb2243d0ec7f737bce0d6dba58187592a390775
Vaclav Elias <vaclavelias@gmail.com>
2025-04-26 19:59:46 +0100

    feat: Cubes generation added

---
+++ examples/code-only/Example17_SignalR/Core/Colours.cs
@@ -0,0 +1,17 @@
+using Example17_SignalR_Shared.Core;
+using Stride.Core.Mathematics;
+
+namespace Example17_SignalR.Core;
+
+public static class Colours
+{
+    public static readonly Dictionary<EntityType, Color> ColourTypes = new()
+    {
+        { EntityType.Success, new Color(25, 135, 84)},
+        { EntityType.Danger, new Color(220, 53, 69) },
+        { EntityType.Warning, new Color(255, 193, 7) },
+        { EntityType.Primary, new Color(13, 110, 253) },
+        { EntityType.Info, new Color(13, 202, 240) },
+        { EntityType.Secondary, new Color(108, 117, 125) }
+    };
+}
--- examples/code-only/Example17_SignalR/Scripts/ScreenManagerScript.cs
+++ examples/code-only/Example17_SignalR/Scripts/ScreenManagerScript.cs
@@ -1,18 +1,31 @@
 using Example17_SignalR.Core;
 using Example17_SignalR.Services;
+using Example17_SignalR_Shared.Core;
 using Example17_SignalR_Shared.Dtos;
+using Stride.CommunityToolkit.Bepu;
+using Stride.CommunityToolkit.Engine;
+using Stride.CommunityToolkit.Helpers;
+using Stride.CommunityToolkit.Rendering.ProceduralModels;
+using Stride.Core.Mathematics;
 using Stride.Engine;
 using Stride.Engine.Events;
+using Stride.Rendering;
+using Stride.Rendering.Materials;
+using Stride.Rendering.Materials.ComputeColors;
 
 namespace Example17_SignalR.Scripts;
 
 public class ScreenManagerScript : AsyncScript
 {
+    private readonly Dictionary<EntityType, Material> _materials = [];
+
     public override async Task Execute()
     {
         var screenService = Services.GetService<ScreenService>();
 
         if (screenService == null) return;
+
+        AddMaterials();
 
         try
         {
@@ -35,14 +48,77 @@ public class ScreenManagerScript : AsyncScript
 
             // This example will be checking if the event is received
             // the rest of the code will be executed every frame
-            if (countReceiver.TryReceive(out var result))
+            if (countReceiver.TryReceive(out var countDto))
             {
-                var formattedMessage = $"From Script: {result.Type}: {result.Count}";
-
-                Console.WriteLine(formattedMessage);
+                CreatePrimitives(countDto);
             }
 
             await Script.NextFrame();
         }
+    }
+
+    private void CreatePrimitives(CountDto countDto)
+    {
+        var formattedMessage = $"From Script: {countDto.Type}: {countDto.Count}";
+
+        Console.WriteLine(formattedMessage);
+
+        for (var i = 0; i < countDto.Count; i++)
+        {
+            var entity = Game.Create3DPrimitive(PrimitiveModelType.Cube,
+                new()
+                {
+                    EntityName = $"Entity",
+                    Material = _materials[countDto.Type],
+                });
+
+            entity.Transform.Position = VectorHelper.RandomVector3([-5, 5], [5, 10], [-5, 5]);
+            entity.Scene = Entity.Scene;
+        }
+    }
+
+    private void AddMaterials()
+    {
+        foreach (var colorType in Colours.ColourTypes)
+        {
+            var material = CreateMaterial(colorType.Value);
+
+            _materials.Add(colorType.Key, material);
+        }
+    }
+
+    public Material CreateMaterial(Color? color = null, float specular = 1.0f, float microSurface = 0.65f)
+    {
+        var materialDescription2 = new MaterialDescriptor
+        {
+            Attributes =
+                {
+                    Diffuse = new MaterialDiffuseMapFeature(new ComputeColor(color ?? GameDefaults.DefaultMaterialColor)),
+                    //DiffuseModel = new MaterialLightmapModelFeature(),
+                    Specular =  new MaterialMetalnessMapFeature(new ComputeFloat(specular)),
+                    SpecularModel = new MaterialSpecularMicrofacetModelFeature(),
+                    MicroSurface = new MaterialGlossinessMapFeature(new ComputeFloat(microSurface))
+                }
+        };
+
+        var materialDescription = new MaterialDescriptor
+        {
+            Attributes =
+                {
+                    Diffuse = new MaterialDiffuseMapFeature(new ComputeColor(color ?? GameDefaults.DefaultMaterialColor)),
+                    DiffuseModel = new MaterialDiffuseLambertModelFeature(),
+                    Specular =  new MaterialMetalnessMapFeature(new ComputeFloat(0)),
+                    SpecularModel = new MaterialSpecularMicrofacetModelFeature()
+                    {
+                        Fresnel = new MaterialSpecularMicrofacetFresnelSchlick(),
+                        Visibility = new MaterialSpecularMicrofacetVisibilitySmithSchlickGGX(),
+                        NormalDistribution = new MaterialSpecularMicrofacetNormalDistributionGGX(),
+                        Environment = new MaterialSpecularMicrofacetEnvironmentGGXLUT(),
+                    },
+                    MicroSurface = new MaterialGlossinessMapFeature(new ComputeFloat(0)),
+                }
+        };
+
+        return Material.New(Game.GraphicsDevice, materialDescription);
     }
 }
