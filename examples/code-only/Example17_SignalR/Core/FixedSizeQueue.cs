887a4427b41ce131d398671c754c00f02099a3a9
Vaclav Elias <vaclavelias@gmail.com>
2025-04-29 23:57:24 +0100

    feat: FixedSizeQueue - Introduce a new fixed-size queue class
    - Adds a new class `FixedSizeQueue` in the `Example17_SignalR.Core` namespace.
    - Manages a fixed-size queue of `MessageDto` objects.
    - Includes a constructor that initializes the queue with a specified maximum size.
    - Implements the `Enqueue(MessageDto item)` method to add items while ensuring the queue does not exceed its maximum size by dequeuing the oldest item if necessary.
    - Provides the `AsSpan()` method to return a read-only span of the items in the queue.

---
+++ examples/code-only/Example17_SignalR/Core/FixedSizeQueue.cs
@@ -0,0 +1,30 @@
+using Example17_SignalR_Shared.Dtos;
+
+namespace Example17_SignalR.Core;
+
+public class FixedSizeQueue
+{
+    public int Count => _queue.Count;
+
+    private readonly Queue<MessageDto> _queue;
+    private readonly int _maxSize;
+
+    public FixedSizeQueue(int maxSize)
+    {
+        _maxSize = maxSize;
+        _queue = new Queue<MessageDto>(maxSize);
+    }
+    public void Enqueue(MessageDto item)
+    {
+        if (_queue.Count == _maxSize)
+        {
+            _queue.Dequeue();
+        }
+        _queue.Enqueue(item);
+    }
+
+    public ReadOnlySpan<MessageDto> AsSpan()
+    {
+        return new ReadOnlySpan<MessageDto>(_queue.ToArray());
+    }
+}
