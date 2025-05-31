949a9cb6384397735ed5ae681ba0f63d7715f87c
Vaclav Elias <vaclavelias@gmail.com>
2025-05-31 19:02:01 +0100

    refactor: Code readability improvements

--- examples/code-only/Example18_Box2DPhysics/Program.cs
+++ examples/code-only/Example18_Box2DPhysics/Program.cs
@@ -124,6 +124,12 @@ void ProcessInput(Scene scene)
 
         SetCubeCount(scene);
     }
+    else if (game.Input.IsKeyPressed(Keys.J))
+    {
+        Add2DShapesWithConstraint(scene, 10);
+
+        SetCubeCount(scene);
+    }
     else if (game.Input.IsKeyReleased(Keys.X))
     {
         foreach (var entity in scene.Entities.Where(w => w.Name.EndsWith(ShapeName) || w.Name == "Cube").ToList())
@@ -183,77 +189,51 @@ void ProcessMouseInput(Scene scene)
 
 void Add2DShapes(Scene scene, Primitive2DModelType? type = null, int count = 5)
 {
-    //var entity = new Entity();
-
     for (int i = 1; i <= count; i++)
     {
         var shapeModel = Get2DShape(type);
 
         if (shapeModel == null) return;
 
-        var entity = game.Create2DPrimitive(shapeModel.Type,
-            new()
-            {
-                Size = shapeModel.Size,
-                Depth = Depth,
-                Material = game.CreateFlatMaterial(shapeModel.Color)
-            });
+        var entity = CreateEntity(scene, shapeModel);
 
-        entity.Name = $"{shapeModel.Type}-{ShapeName}";
-        entity.Transform.Position = GetRandomPosition();
-        entity.Scene = scene;
+        var bodyId = box2DSimulation.CreateDynamicBody(entity, entity.Transform.Position);
 
-        var bodyId2 = box2DSimulation.CreateDynamicBody(entity, entity.Transform.Position);
-
-        // Create shape for the body
-        var shapeDef = b2DefaultShapeDef();
-        shapeDef.density = 1.0f;
-        shapeDef.material.friction = 0.3f;
-        if (shapeModel.Type == Primitive2DModelType.Square2D || shapeModel.Type == Primitive2DModelType.Rectangle2D)
-        {
-            var box = b2MakeBox(shapeModel.Size.X / 2, shapeModel.Size.Y / 2);
-            b2CreatePolygonShape(bodyId2, ref shapeDef, ref box);
-        }
-        else if (shapeModel.Type == Primitive2DModelType.Circle2D)
-        {
-            var circle = new B2Circle(new B2Vec2(0.0f, 0.0f), shapeModel.Size.X);
-            b2CreateCircleShape(bodyId2, ref shapeDef, ref circle);
-        }
-        else if (shapeModel.Type == Primitive2DModelType.Triangle2D)
-        {
-            var meshData = TriangleProceduralModel.New(shapeModel.Size);
-            var points2 = meshData.Vertices.Select(v => new B2Vec2(v.Position.X, v.Position.Y)).ToArray();
-
-            //// Define the three vertices of your triangle
-            //// For an equilateral triangle centered at origin with size as the width
-            //float halfWidth = shapeModel.Size.X / 2;
-            //float halfHeight = shapeModel.Size.Y / 2;
-
-            //B2Vec2[] points = new B2Vec2[3] {
-            //    new B2Vec2(0, halfHeight),            // Top vertex
-            //    new B2Vec2(-halfWidth, -halfHeight),  // Bottom left
-            //    new B2Vec2(halfWidth, -halfHeight)    // Bottom right
-            //};
-
-            // Create a hull from these points
-            B2Hull hull = b2ComputeHull(points2, 3);
-
-            // Create a polygon shape from the hull
-            B2Polygon triangle = b2MakePolygon(ref hull, 0.0f);
-
-            // Create the shape on the body
-            b2CreatePolygonShape(bodyId2, ref shapeDef, ref triangle);
-        }
-        else if (shapeModel.Type == Primitive2DModelType.Capsule)
-        {
-            var capsule = new B2Capsule(
-                new(0, -shapeModel.Size.X / 2),
-                new(0, (shapeModel.Size.Y / 2) - shapeModel.Size.X / 2),
-                shapeModel.Size.X / 2);
-
-            b2CreateCapsuleShape(bodyId2, ref shapeDef, ref capsule);
-        }
+        Create2DShapePhysics(shapeModel, bodyId);
     }
+}
+
+void Add2DShapesWithConstraint(Scene scene, int count = 5)
+{
+    for (int i = 1; i <= count; i++)
+    {
+        var shapeModel1 = Get2DShape();
+        var shapeModel2 = Get2DShape();
+
+        if (shapeModel1 == null || shapeModel2 == null) return;
+
+        var entity1 = CreateEntity(scene, shapeModel1);
+
+        var bodyId = box2DSimulation.CreateDynamicBody(entity1, entity1.Transform.Position);
+
+        Create2DShapePhysics(shapeModel1, bodyId);
+    }
+}
+
+Entity CreateEntity(Scene scene, Shape2DModel shape)
+{
+    var entity = game.Create2DPrimitive(shape.Type, new()
+    {
+        Size = shape.Size,
+        Depth = Depth,
+        Material = game.CreateFlatMaterial(shape.Color)
+    });
+
+    entity.Name = $"{shape.Type}-{ShapeName}";
+    entity.Transform.Position = GetRandomPosition();
+    entity.Scene = scene;
+
+    return entity;
 }
 
 void AddBoxEntityWithPhysics(Scene scene)
@@ -323,3 +303,55 @@ void RenderNavigation()
 }
 
 static Vector3 GetRandomPosition() => new(Random.Shared.Next(-5, 5), Random.Shared.Next(10, 30), 0);
+
+static void Create2DShapePhysics(Shape2DModel shapeModel, B2BodyId bodyId)
+{
+    // Create shape for the body
+    var shapeDef = b2DefaultShapeDef();
+    shapeDef.density = 1.0f;
+    shapeDef.material.friction = 0.3f;
+    if (shapeModel.Type == Primitive2DModelType.Square2D || shapeModel.Type == Primitive2DModelType.Rectangle2D)
+    {
+        var box = b2MakeBox(shapeModel.Size.X / 2, shapeModel.Size.Y / 2);
+        b2CreatePolygonShape(bodyId, ref shapeDef, ref box);
+    }
+    else if (shapeModel.Type == Primitive2DModelType.Circle2D)
+    {
+        var circle = new B2Circle(new B2Vec2(0.0f, 0.0f), shapeModel.Size.X);
+        b2CreateCircleShape(bodyId, ref shapeDef, ref circle);
+    }
+    else if (shapeModel.Type == Primitive2DModelType.Triangle2D)
+    {
+        var meshData = TriangleProceduralModel.New(shapeModel.Size);
+        var points2 = meshData.Vertices.Select(v => new B2Vec2(v.Position.X, v.Position.Y)).ToArray();
+
+        //// Define the three vertices of your triangle
+        //// For an equilateral triangle centered at origin with size as the width
+        //float halfWidth = shapeModel.Size.X / 2;
+        //float halfHeight = shapeModel.Size.Y / 2;
+
+        //B2Vec2[] points = new B2Vec2[3] {
+        //    new B2Vec2(0, halfHeight),            // Top vertex
+        //    new B2Vec2(-halfWidth, -halfHeight),  // Bottom left
+        //    new B2Vec2(halfWidth, -halfHeight)    // Bottom right
+        //};
+
+        // Create a hull from these points
+        B2Hull hull = b2ComputeHull(points2, 3);
+
+        // Create a polygon shape from the hull
+        B2Polygon triangle = b2MakePolygon(ref hull, 0.0f);
+
+        // Create the shape on the body
+        b2CreatePolygonShape(bodyId, ref shapeDef, ref triangle);
+    }
+    else if (shapeModel.Type == Primitive2DModelType.Capsule)
+    {
+        var capsule = new B2Capsule(
+            new(0, -shapeModel.Size.X / 2),
+            new(0, (shapeModel.Size.Y / 2) - shapeModel.Size.X / 2),
+            shapeModel.Size.X / 2);
+
+        b2CreateCapsuleShape(bodyId, ref shapeDef, ref capsule);
+    }
+}
