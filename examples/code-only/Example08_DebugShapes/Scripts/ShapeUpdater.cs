c1bca78b9bff1ca1363c35cfa314ba1b90cc0a05
Vaclav Elias <vaclavelias@gmail.com>
2025-08-16 23:11:10 +0100

    refactor: ScreenPositionToWorldPositionRaycast removed

--- examples/code-only/Example08_DebugShapes/Scripts/ShapeUpdater.cs
+++ examples/code-only/Example08_DebugShapes/Scripts/ShapeUpdater.cs
@@ -1,3 +1,4 @@
+using Stride.CommunityToolkit.Bullet;
 using Stride.CommunityToolkit.DebugShapes.Code;
 using Stride.Core;
 using Stride.Core.Collections;
@@ -314,7 +315,9 @@ public class ShapeUpdater : SyncScript
         if (Input.IsMouseButtonPressed(Stride.Input.MouseButton.Left) && CurrentCamera != null)
         {
             var clickPos = Input.MousePosition;
-            var result = ScreenPositionToWorldPositionRaycast(clickPos, CurrentCamera, this.GetSimulation());
+
+            var result = CurrentCamera.Raycast(this.GetSimulation(), clickPos);
+
             if (result.Succeeded)
             {
                 var cameraWorldPos = CurrentCamera.Entity.Transform.WorldMatrix.TranslationVector;
@@ -350,36 +353,5 @@ public class ShapeUpdater : SyncScript
         // center cone thing yes
         DebugDraw.DrawCone(new Vector3(0, 0.5f, 0), 2.0f, 0.5f, color: Color.HotPink);
 
-    }
-
-    // ToDo, this method is already in CameraComponentExtensions?
-    public static HitResult ScreenPositionToWorldPositionRaycast(Vector2 screenPos, CameraComponent camera, Simulation simulation)
-    {
-        var invertedViewProjection = Matrix.Invert(camera.ViewProjectionMatrix);
-
-        // Reconstruct the projection-space position in the (-1, +1) range.
-        // Don't forget that Y is down in screen coordinates, but up in projection space
-        Vector3 sPos;
-        sPos.X = screenPos.X * 2f - 1f;
-        sPos.Y = 1f - screenPos.Y * 2f;
-
-        // Compute the near (start) point for the raycast
-        // It's assumed to have the same projection space (x,y) coordinates and z = 0 (lying on the near plane)
-        // We need to unproject it to world space
-        sPos.Z = 0f;
-        var vectorNear = Vector3.Transform(sPos, invertedViewProjection);
-        vectorNear /= vectorNear.W;
-
-        // Compute the far (end) point for the raycast
-        // It's assumed to have the same projection space (x,y) coordinates and z = 1 (lying on the far plane)
-        // We need to unproject it to world space
-        sPos.Z = 1f;
-        var vectorFar = Vector3.Transform(sPos, invertedViewProjection);
-        vectorFar /= vectorFar.W;
-
-        // Raycast from the point on the near plane to the point on the far plane and get the collision result
-        var result = simulation.Raycast(vectorNear.XYZ(), vectorFar.XYZ());
-
-        return result;
     }
 }
