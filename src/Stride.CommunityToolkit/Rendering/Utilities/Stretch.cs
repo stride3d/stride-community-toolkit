eae5d19c7589605bac487b2aedde783e471e2ab3
Vaclav Elias <4528464+VaclavElias@users.noreply.github.com>
2025-08-31 12:09:00 +0100

    refactor: Update enums and remove partial TextureCanvas class
    - Changed the class declaration in `TextureCanvas.cs` from `public partial class TextureCanvas : IDisposable` to `public class TextureCanvas : IDisposable`, indicating it is no longer a partial class while retaining its private fields.

--- src/Stride.CommunityToolkit/Rendering/Utilities/Stretch.cs
+++ src/Stride.CommunityToolkit/Rendering/Utilities/Stretch.cs
@@ -1,32 +1,29 @@
-﻿namespace Stride.CommunityToolkit.Rendering.Utilities;
+namespace Stride.CommunityToolkit.Rendering.Utilities;
 
-public partial class TextureCanvas
+/// <summary>
+///     Stretch modes when textures to a <see cref="TextureCanvas" />
+/// </summary>
+public enum Stretch
 {
     /// <summary>
-    ///     Stretch modes when textures to a <see cref="TextureCanvas" />
+    ///     The texture preserves its original size. Overflowing content is cropped.
     /// </summary>
-    public enum Stretch
-    {
-        /// <summary>
-        ///     The texture preserves its original size. Overflowing content is cropped.
-        /// </summary>
-        None,
+    None,
 
-        /// <summary>
-        ///     The texture is resized to fill the destination dimensions. The aspect ratio is not preserved.
-        /// </summary>
-        Stretch,
+    /// <summary>
+    ///     The texture is resized to fill the destination dimensions. The aspect ratio is not preserved.
+    /// </summary>
+    Stretch,
 
-        /// <summary>
-        ///     The texture is resized to fit in the destination dimensions while it preserves its native aspect ratio.
-        /// </summary>
-        Contain,
+    /// <summary>
+    ///     The texture is resized to fit in the destination dimensions while it preserves its native aspect ratio.
+    /// </summary>
+    Contain,
 
-        /// <summary>
-        ///     The texture is resized to fill the destination dimensions while it preserves its native aspect ratio. If the aspect
-        ///     ratio of the destination rectangle differs from the source, the source texture is clipped to fit in the destination
-        ///     dimensions.
-        /// </summary>
-        Cover
-    }
+    /// <summary>
+    ///     The texture is resized to fill the destination dimensions while it preserves its native aspect ratio. If the aspect
+    ///     ratio of the destination rectangle differs from the source, the source texture is clipped to fit in the destination
+    ///     dimensions.
+    /// </summary>
+    Cover
 }
