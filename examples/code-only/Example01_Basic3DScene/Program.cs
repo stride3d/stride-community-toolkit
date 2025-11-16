8a521dd980dfa67e21a289e05e9f801a0cb256d9
Vaclav Elias <4528464+VaclavElias@users.noreply.github.com>
2025-11-16 20:29:42 +0000

    feat: Program.cs - Refactor game setup
    - Refactored `game.Run` to use a new `Start(Scene rootScene)` method instead of an inline lambda.
    - Added `Start()` method to encapsulate game setup logic.

--- examples/code-only/Example01_Basic3DScene/Program.cs
+++ examples/code-only/Example01_Basic3DScene/Program.cs
@@ -7,17 +7,17 @@ using Stride.Engine;
 
 using var game = new Game();
 
-game.Run(start: (Scene rootScene) =>
+game.Run(start: Start);
+
+void Start(Scene rootScene)
 {
     game.SetupBase3DScene();
     game.AddSkybox();
 
     var entity = game.Create3DPrimitive(PrimitiveModelType.Capsule);
-
     entity.Transform.Position = new Vector3(0, 8, 0);
-
     entity.Scene = rootScene;
-});
+}
 
 /*
 ---example-metadata
